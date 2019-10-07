e951eb509eb9e35105859d9d0d34f540f452a744
Francisco Fuster Tormo <ffuster@carrerasresearch.org>
2019-10-07 09:23:36 +0200

    Modified doGGplot.R to print only one copy number count. Adapted to the python call of the script

--- doGGplot.R
+++ doGGplot.R
@@ -1,5 +1,3 @@
-library(ggplot2)
-
 #########################################################################
 #Creates a ggplot from a table file passed as parameter.                #
 #The structure of the table should be:                                  #
@@ -10,18 +8,22 @@ library(ggplot2)
 #       CN is the copy number value of the segment                      #
 #       type is the type of program that has inferred this segment      #
 #########################################################################
-sysarg <- commandArgs(trailingOnly = TRUE)
+arg <- commandArgs(trailingOnly = TRUE)
 
-fil = sysarg[1][1]
+if (length(arg) >= 2) {
+  fil <- arg[1]
+  output <- arg[2]
+} else if (length(arg) == 1) {
+  fil <- arg[1]
+  output <- "CN.png"
+} else {
+  stop("ERROR: Not enough arguments passed to R script.\nUsage:\n\tRscript doGGplot.R input_file.tsv [output_file.png]")
+}
 
-#gg <- read.table("tab4ggplot_TCN.tsv", sep = "\t", header = TRUE)
-#gg$chr <- as.numeric(gg$chr)
-#ggplot(data=gg, aes(x=start, xend=end, y=cn, yend=cn, color=type)) + geom_segment() + facet_grid(.~chr) + coord_cartesian(ylim = c(0,7)) + 
-#  labs(title="Total copy number", x="Chromosomal_positions", y="Total_copy_number_counts")
-#ggsave("Total_CN.png", width = 8.27) #DIN-A4 format
+library(ggplot2)
 
-gg2 <- read.table(fil, sep = "\t", header = TRUE)
-gg2$chr <- as.numeric(gg2$chr)
-ggplot(data=gg2, aes(x=start, xend=end, y=cn, yend=cn, color=type)) + geom_segment() + facet_grid(.~chr) + coord_cartesian(ylim = c(0,4)) + 
-  labs(title="Minor copy number", x="Chromosomal_positions", y="Minor_copy_number_counts")
-ggsave("Minor_CN.png", width = 8.27)
+gg <- read.table(fil, sep = "\t", header = TRUE)
+gg$chr <- as.numeric(gg$chr)
+ggplot(data=gg, aes(x=start, xend=end, y=cn, yend=cn, color=type)) + geom_segment() + facet_grid(.~chr) + coord_cartesian(ylim = c(0,4)) + 
+  labs(title="Copy number", x="Chromosomal_positions", y="Copy_number_counts")
+ggsave(output, width = 8.27) #DIN-A4 format
