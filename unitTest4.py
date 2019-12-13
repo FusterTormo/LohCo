3644c21bae29dc806e80852338b361cd22fa111b
Francisco Fuster Tormo <ffuster@carrerasresearch.org>
2019-12-13 18:17:50 +0100

    Created a unit test for the introduction of Sequenza data

---
+++ unitTest4.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+MAIN: Example of introduction of sequenza
+"""
+
+import libcomparison as lc
+
+sequenza = lc.convert2region("input_examples/sequenzaExample/TCGA-04-1343_segments.txt", "sequenza")
+facets = lc.convert2region("input_examples/TCGA-04-1343/TCGA-04-1343_9ece2e42_VS_65188600/facets_comp_cncf.tsv", "facets")
+ascat = lc.convert2region("input_examples/TCGA-04-1343/TCGA-04-1343_9ece2e42_VS_65188600/H_GP-04-1343-01A-01W-0488-09-1.copynumber.caveman.csv", "ascatngs")
+print("INFO: Arxius oberts satisfactoriament")
