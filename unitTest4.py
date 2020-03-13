d813799d80297a339bfbf4567dd7cda751d68029
fustertormo <emailserios@gmail.com>
2020-03-13 16:19:07 +0100

    Defined the unit test 4

--- unitTest4.py
+++ unitTest4.py
@@ -5,11 +5,31 @@
 MAIN: Example of introduction of sequenza
 """
 
-import libcomparison as lc
+"""
+The test includes:
+    1. Open the sample outputs from sequenza, facets, ascatNGS, and array respectively
+    2. Compare all tools against arrays in the whole genome
+    3. Get the copy number reported by the 4 tools in specific regions: BRCA1, BRCA2, PALB2, and ATM
+    Estadistiques?????
+"""
 
-# sequenza = lc.convert2region("input_examples/sequenzaExample/TCGA-04-1343_segments.txt", "sequenza")
-# facets = lc.convert2region("input_examples/TCGA-04-1343/TCGA-04-1343_9ece2e42_VS_65188600/facets_comp_cncf.tsv", "facets")
-# ascat = lc.convert2region("input_examples/TCGA-04-1343/TCGA-04-1343_9ece2e42_VS_65188600/H_GP-04-1343-01A-01W-0488-09-1.copynumber.caveman.csv", "ascatngs")
+import libcomparison as lc
+import libgetters as lg
+
+sequenza = lc.convert2region("../9793255c_VS_21fc93b7_Sequenza/TCGA-04-1332_segments.txt", "sequenza")
+facets = lc.convert2region("../9793255c_VS_f4b549d0_FACETS/facets_comp_cncf.tsv", "facets")
+ascat = lc.convert2region("../90cf56c6_VS_f4b549d0_ASCAT/H_GP-04-1332-01A-01W-0488-09-1.copynumber.caveman.csv", "ascatngs")
 print("INFO: Arxius oberts satisfactoriament")
 
-lc.getFragments(None, None, [["2", 12341, 1234123474], ["2", 43234, 44444]])
+#Regions d'interes. Dades obtingudes des de biogps
+brca1 = ["17", 43044295, 43170245]
+brca2 = ["13", 32315086, 32400266]
+palb2 = ["16", 23603160, 23641310]
+atm = ["11", 108222484, 108369102]
+
+# print(brca1[1:])
+print(lg.getCopyNumber(brca1[1:3], brca1[0], sequenza))
+print(lg.getCopyNumber(brca1[1:3], brca1[0], facets))
+print(lg.getCopyNumber(brca1[1:3], brca1[0], ascat))
+
+#lc.getFragments(None, None, [["2", 12341, 1234123474], ["2", 4334, 4444], ["3", 1001, 24000]])
