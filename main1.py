2a96d907ab15c11bc5ec8bceea0ebaf72ca2d31e
FusterTormo <franfustor@gmail.com>
2020-03-17 09:25:46 +0100

    Update main1.py
    Written schema of final output in main1

--- main1.py
+++ main1.py
@@ -5,9 +5,14 @@
 Llegir summary.md on hi ha les variants detectades en cadascuna de les mostres de OV i les mutacions que tenen en BRCA
 Llegir els corresponents FACETS, ascatNGS i Sequenza
 Comprovar que detecten en la regio. Si LOH o no
-Fer una taula amb tota la informacio.
-    Mostra uuid variant_detectada variant_caller es_positiu? outputFACETS outputAscatNGS outputSequenza
-Fer un resum de la taula amb tota la informacio
+-----------------------------------------------
+IDEA: Fer una taula amb tota la informacio.
+
+	|Mostra|uuids[0:8]|Variant      |output FACETS        |output ascatNGS      |output Sequenza      |
+	|-     |__VS__    |tumor|control|BRCA1|BRCA2|ATM|PALB2|BRCA1|BRCA2|ATM|PALB2|BRCA1|BRCA2|ATM|PALB2|
+	|------|----------|-----|-------|-----|-----|---|-----|-----|-----|---|-----|-----|-----|---|-----|
+
+? Fer un resum de la taula amb tota la informacio
     Positius FACETS_positius ascatNGS_positius Sequenza_positius
     Negatius FACETS_negatius ascatNGS_negatius Sequenza_negatius
-""""
+"""
