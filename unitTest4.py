2ea3ec39b9d060fa89d67c25a9811b9b3104398e
fustertormo <emailserios@gmail.com>
2020-03-17 10:44:38 +0100

    Checked that the outputs from Sequenza are correct

--- unitTest4.py
+++ unitTest4.py
@@ -10,15 +10,17 @@ The test includes:
     1. Open the sample outputs from sequenza, facets, ascatNGS, and array respectively
     2. Compare all tools against arrays in the whole genome
     3. Get the copy number reported by the 4 tools in specific regions: BRCA1, BRCA2, PALB2, and ATM
-    Estadistiques?????
 """
 
 import libcomparison as lc
 import libgetters as lg
 import libstatistics as ls
+import libconstants
 
 # Open the output for the sample TCGA-04-1332 output from all the tools
 sequenza = lc.convert2region("../9793255c_VS_21fc93b7_Sequenza/TCGA-04-1332_segments.txt", "sequenza")
+print(sequenza["purity"])
+print(sequenza["ploidy"])
 facets = lc.convert2region("../9793255c_VS_f4b549d0_FACETS/facets_comp_cncf.tsv", "facets")
 ascat = lc.convert2region("../90cf56c6_VS_f4b549d0_ASCAT/H_GP-04-1332-01A-01W-0488-09-1.copynumber.caveman.csv", "ascatngs")
 array = lc.convert2region("../73a3a9bb-7dfc-4fc5-9f31-b2630c82010b_Array/QUANT_p_TCGA_Batch12_AFFX_GenomeWideSNP_6_F05_437768.grch38.seg.v2.txt", "array")
@@ -36,6 +38,7 @@ fragments = lc.getFragments(array, sequenza)
 tab = lc.doComparison(fragments, array, sequenza)
 c1, c2 = ls.calculateCounts(tab)
 contingency = ls.doContingency(tab, ["A", "D", "N"])
+print(ls.printTable(tab, "Array", "Sequenza", False))
 
 
 #Regions d'interes. Dades obtingudes des de biogps
