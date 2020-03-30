bd5759b4314c73a9a6fc2c596b96e5b81f9ba2e6
fustertormo <emailserios@gmail.com>
2020-03-30 15:22:58 +0200

    Afegit per mesclar amb unitTest4

---
+++ unitTestAux.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+MAIN: Example of introduction of sequenza
+"""
+
+"""
+The test includes:
+    1. Open the sample outputs from sequenza, facets, ascatNGS, and array respectively
+    2. Compare all tools against arrays in the whole genome
+    3. Get the copy number reported by the 4 tools in specific regions: BRCA1, BRCA2, PALB2, and ATM
+"""
+
+import libcomparison as lc
+import libgetters as lg
+import libstatistics as ls
+import sys
+
+# Open the output for the sample TCGA-04-1332 output from all the tools
+sequenza = lc.convert2region("../9793255c_VS_21fc93b7_Sequenza/TCGA-04-1332_segments.txt", "sequenza")
+facets = lc.convert2region("../9793255c_VS_f4b549d0_FACETS/facets_comp_cncf.tsv", "facets")
+ascat = lc.convert2region("../90cf56c6_VS_f4b549d0_ASCAT/H_GP-04-1332-01A-01W-0488-09-1.copynumber.caveman.csv", "ascatngs")
+array = lc.convert2region("../73a3a9bb-7dfc-4fc5-9f31-b2630c82010b_Array/QUANT_p_TCGA_Batch12_AFFX_GenomeWideSNP_6_F05_437768.grch38.seg.v2.txt", "array")
+print("INFO: Arxius oberts satisfactoriament")
+
+print("\nINFO: Resum de les dades obteses en cada eina")
+car = ls.countsXtool(array)
+cs = ls.countsXtool(sequenza)
+cf = ls.countsXtool(facets)
+cas = ls.countsXtool(ascat)
+print("\t A\t| N \t| L \t| D")
+print("Array \t {amp}\t| {normal}\t| {loh}\t| {dele}".format(amp = car["A"], normal = car["N"], loh = car["L"], dele = car["D"]))
+print("FACETS\t {amp}\t| {normal}\t| {loh}\t| {dele}".format(amp = cf["A"], normal = cf["N"], loh = cf["L"], dele = cf["D"]))
+print("ascatN\t {amp}\t| {normal}\t| {loh}\t| {dele}".format(amp = cas["A"], normal = cas["N"], loh = cas["L"], dele = cas["D"]))
+print("Sequen\t {amp}\t| {normal}\t| {loh}\t| {dele}".format(amp = cs["A"], normal = cs["N"], loh = cs["L"], dele = cs["D"]))
+
+print("\nINFO: Comparant les dades amb els arrays")
+fragments = lc.getFragments(array, facets)
+tab = lc.doComparison(fragments, array, facets)
+c1, c2 = ls.calculateCounts(tab)
+cAvF = ls.doContingency(tab, ["A", "D", "N"])
+tabAvF = ls.printTable(tab, "Array", "FACETS", False).split("\n")
+
+fragments = lc.getFragments(array, ascat)
+tab = lc.doComparison(fragments, array, ascat)
+cAvA = ls.doContingency(tab, ["A", "D", "N"])
+tabAvA = ls.printTable(tab, "Array", "ascatNGS", False).split("\n")
+
+fragments = lc.getFragments(array, sequenza)
+tab = lc.doComparison(fragments, array, sequenza)
+cAvS = ls.doContingency(tab, ["A", "D", "N"])
+tabAvS = ls.printTable(tab, "Array", "Sequenza", False).split("\n")
+print("{}\t\t\t\t||{}\t\t\t||{}".format(tabAvF[0], tabAvA[0], tabAvS[0]))
+print("{}\t\t||{}\t\t||{}".format(tabAvF[1], tabAvA[1], tabAvS[1]))
+print("{}\t\t||{}\t\t||{}".format(tabAvF[2], tabAvA[2], tabAvS[2]))
+print("{}\t\t||{}\t\t||{}".format(tabAvF[3], tabAvA[3], tabAvS[3]))
+print("{}\t\t||{}\t\t||{}".format(tabAvF[4], tabAvA[4], tabAvS[4]))
+print("{}\t\t||{}\t\t||{}".format(tabAvF[5], tabAvA[5], tabAvS[5]))
+print("=====================================================================")
+print("TODO: Add information regarding the contingency table for the alterations (A, D, N)")
+
+summaryFile = "../summary.md"
+with open(summaryFile, "r") as fi :
+	for l in fi :
+		if l.startswith("TCGA-04-1332") :
+			print(l)
+	
+
+#Regions d'interes. Dades obtingudes des de biogps
+brca1 = ["17", 43044295, 43170245]
+brca2 = ["13", 32315086, 32400266]
+palb2 = ["16", 23603160, 23641310]
+atm = ["11", 108222484, 108369102]
+
+# TODO Consultar summary.md per vore si la mostra te mutacions en BRCA
+
+print("\nINFO: Comparant el LOH en BRCA1")
+print("Array output: {}".format(lg.getCopyNumber(brca1[1:3], brca1[0], array)))
+print("Sequenza output: {}".format(lg.getCopyNumber(brca1[1:3], brca1[0], sequenza)))
+print("FACETS output: {}".format(lg.getCopyNumber(brca1[1:3], brca1[0], facets)))
+print("ascatNGS output: {}".format(lg.getCopyNumber(brca1[1:3], brca1[0], ascat)))
+
+print("\nINFO: Comparant el LOH en BRCA2")
+print("Array output: {}".format(lg.getCopyNumber(brca2[1:3], brca2[0], array)))
+print("Sequenza output: {}".format(lg.getCopyNumber(brca2[1:3], brca2[0], sequenza)))
+print("FACETS output: {}".format(lg.getCopyNumber(brca2[1:3], brca2[0], facets)))
+print("ascatNGS output: {}".format(lg.getCopyNumber(brca2[1:3], brca2[0], ascat)))
+
+print("\nINFO: Comparant el LOH ATM")
+print("Array output: {}".format(lg.getCopyNumber(atm[1:3], atm[0], array)))
+print("Sequenza output: {}".format(lg.getCopyNumber(atm[1:3], atm[0], sequenza)))
+print("FACETS output: {}".format(lg.getCopyNumber(atm[1:3], atm[0], facets)))
+print("ascatNGS output: {}".format(lg.getCopyNumber(atm[1:3], atm[0], ascat)))
+
+print("\nINFO: Comparant el LOH en PALB2")
+print("Array output: {}".format(lg.getCopyNumber(palb2[1:3], palb2[0], array)))
+print("Sequenza output: {}".format(lg.getCopyNumber(palb2[1:3], palb2[0], sequenza)))
+print("FACETS output: {}".format(lg.getCopyNumber(palb2[1:3], palb2[0], facets)))
+print("ascatNGS output: {}".format(lg.getCopyNumber(palb2[1:3], palb2[0], ascat)))
