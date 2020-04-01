c2c7b25400d3346bec1af7d3b576a71ea81c0b23
FusterTormo <franfustor@gmail.com>
2020-04-01 15:25:00 +0200

    Testing if Sequenza has been executed in the folder

--- main1.py
+++ main1.py
@@ -18,9 +18,11 @@ IDEA: Fer una taula amb tota la informacio.
 """
 
 import sqlite3
+import os
 
 # Vore l'estatus de les mostres i executar sequenza en aquelles que no s'haja executat
 dbcon = sqlite3.connect("/g/strcombio/fsupek_cancer2/TCGA_bam/info/info.db")
+wd = "/g/strcombio/fsupek_cancer2/TCGA_bam/OV"
 with dbcon :
 	cur = dbcon.cursor()
 	q = cur.execute("SELECT submitter FROM patient WHERE cancer='OV'")
@@ -34,7 +36,20 @@ for c in cases :
 		tumors = q.fetchall()
 		q = cur.execute("SELECT uuid FROM sample WHERE submitter='{}' AND tumor LIKE '%Normal%'".format(c[0]))
 		controls = q.fetchall()
-	print(c)
-	print(tumors)
-	print(controls)
-	
+	for tm in tumors :
+		for cn in controls :
+			aux = tm[0].split("-")[0]
+			aux2 = cn[0].split("-")[0]
+			folder = "{wd}/{sub}/{tumor}_VS_{control}".format(wd = wd, sub = c[0], tumor = aux, control = aux2)
+			# Comprovar l'status de l'analisi de Sequenza
+			aux = "{}_Sequenza".format(folder)
+			print("Comprovant {}".format(aux))
+			if os.path.isfolder("{}_Sequenza".format(folder)) :
+				# Si existeix la carpeta es que s'ha fet el seqz de la mostra. Comprovar si s'ha fet el sequenza
+				aux = "{}_Sequenza".format(folder)
+				if not os.path.isfile("{}/{}_segments.txt".format(aux, c[0])) :
+					aux = "{}/{}_segments.txt".format(aux, c[0])
+					print("Executar plotSequenza en {}".format(aux))
+					break
+			else :
+				print("{} no existeix".format(aux))
