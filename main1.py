7f6665e6f8459603032ada1fa0fa2118a7312069
FusterTormo <franfustor@gmail.com>
2020-04-01 12:52:35 +0200

    Created a function to see the status of the analysis in OV

--- main1.py
+++ main1.py
@@ -16,3 +16,25 @@ IDEA: Fer una taula amb tota la informacio.
     Positius FACETS_positius ascatNGS_positius Sequenza_positius
     Negatius FACETS_negatius ascatNGS_negatius Sequenza_negatius
 """
+
+import sqlite3
+
+# Vore l'estatus de les mostres i executar sequenza en aquelles que no s'haja executat
+dbcon = sqlite3.connect("/g/strcombio/fsupek_cancer2/TCGA_bam/info/info.db")
+with dbcon :
+	cur = dbcon.cursor()
+	q = cur.execute("SELECT submitter FROM patient WHERE cancer='OV'")
+	cases = q.fetchall()
+
+print("INFO: {} cases found".format(len(cases)))
+for c in cases :
+	with dbcon :
+		cur = dbcon.cursor()
+		q = cur.execute("SELECT uuid FROM sample WHERE submitter='{}' AND tumor LIKE '%Tumor%'")
+		tumors = q.fetchall()
+		q = cur.execute("SELECT uuid FROM sample WHERE submitter='{}' AND tumor LIKE '%Normal%'")
+		controls = q.fetchall()
+	print(c)
+	print(tumors)
+	print(controls)
+	break
