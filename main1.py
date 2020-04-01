6e32baddf3898babb47c1d8b4c14d94b9a62d02c
FusterTormo <franfustor@gmail.com>
2020-04-01 16:44:47 +0200

    Added instructions to execute in situ the Sequenza pending analysis
    Added the subprocess instructions to run the sequenza 2nd analysis while checking if the data is already finished. Pending to run an example.

--- main1.py
+++ main1.py
@@ -19,10 +19,13 @@ IDEA: Fer una taula amb tota la informacio.
 
 import sqlite3
 import os
+import sys
+import subprocess
 
 # Vore l'estatus de les mostres i executar sequenza en aquelles que no s'haja executat
 dbcon = sqlite3.connect("/g/strcombio/fsupek_cancer2/TCGA_bam/info/info.db")
 wd = "/g/strcombio/fsupek_cancer2/TCGA_bam/OV"
+sequenza = "/home/ffuster/scripts/plotSequenza.R"
 with dbcon :
 	cur = dbcon.cursor()
 	q = cur.execute("SELECT submitter FROM patient WHERE cancer='OV'")
@@ -30,26 +33,30 @@ with dbcon :
 
 print("INFO: {} cases found".format(len(cases)))
 for c in cases :
+	# Recollir la informacio dels bams i el sexe que te el cas registrats
 	with dbcon :
 		cur = dbcon.cursor()
 		q = cur.execute("SELECT uuid FROM sample WHERE submitter='{}' AND tumor LIKE '%Tumor%'".format(c[0]))
 		tumors = q.fetchall()
 		q = cur.execute("SELECT uuid FROM sample WHERE submitter='{}' AND tumor LIKE '%Normal%'".format(c[0]))
 		controls = q.fetchall()
+		q = cur.execute("SELECT gender FROM patient WHERE submitter='{}'".format(c[0]))
+		gender = q.fetchone()[0]
 	for tm in tumors :
 		for cn in controls :
 			aux = tm[0].split("-")[0]
 			aux2 = cn[0].split("-")[0]
 			folder = "{wd}/{sub}/{tumor}_VS_{control}".format(wd = wd, sub = c[0], tumor = aux, control = aux2)
 			# Comprovar l'status de l'analisi de Sequenza
-			aux = "{}_Sequenza".format(folder)
-			print("Comprovant {}".format(aux))
 			if os.path.isdir("{}_Sequenza".format(folder)) :
 				# Si existeix la carpeta es que s'ha fet el seqz de la mostra. Comprovar si s'ha fet el sequenza
 				aux = "{}_Sequenza".format(folder)
 				if not os.path.exists("{}/{}_segments.txt".format(aux, c[0])) :
-					aux = "{}/{}_segments.txt".format(aux, c[0])
-					print("Executar plotSequenza en {}".format(aux))
+					cmd = "Rscript {seqScript} {cas} {folder} {gender}".format(cas = c[0], folder = aux, gender = gender, seqScript = sequenza)
+					proc = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+					out, err = subprocess.communicate()
+					print(proc.returncode)
 					sys.exit()
+
 			else :
-				print("{} no existeix".format(aux))
+				print("INFO: Analisi no fet: {}".format("{}_Sequenza".format(folder)))
