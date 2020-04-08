b7f6f7a52d91e657ffeebd6528d1feb11da78004
FusterTormo <franfustor@gmail.com>
2020-04-08 08:21:29 +0200

    Updated the error managing while running Sequenza check

--- main1.py
+++ main1.py
@@ -27,6 +27,8 @@ dbcon = sqlite3.connect("/g/strcombio/fsupek_cancer2/TCGA_bam/info/info.db")
 wd = "/g/strcombio/fsupek_cancer2/TCGA_bam/OV"
 sequenza = "/home/ffuster/Scripts/plotSequenza.R"
 cont = 0
+errors = 0
+messages = []
 with dbcon :
 	cur = dbcon.cursor()
 	q = cur.execute("SELECT submitter FROM patient WHERE cancer='OV'")
@@ -60,8 +62,9 @@ for c in cases :
 					proc = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 					out, err = proc.communicate()
 					if proc.returncode != 0 :
+						errors += 1
+						messages.append(cmd)
 						print("ERROR: While runing {}\nDescription:\n{}".format(cmd, err))
-						sys.exit()
 			else :
 				aux = "{wd}/{sub}/{uuid}/{bam}".format(wd = wd, sub = c[0], uuid = tm[0], bam = tm[1])
 				aux2 = "{wd}/{sub}/{uuid}/{bam}".format(wd = wd, sub = c[0], uuid = cn[0], bam = cn[1])
@@ -70,12 +73,20 @@ for c in cases :
 				proc = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 				out, err = proc.communicate()
 				if proc.returncode != 0 :
+					errors += 1
+					messages.append(cmd)
 					print("ERROR: While running {}\nDescription:\n{}".format(cmd, err))
-					sys.exit()
 				else :
 					cmd = "Rscript {seqScript} {cas} {folder} {gender}".format(cas = c[0], folder = seq, gender = gender, seqScript = sequenza)
 					proc = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 					out, err = proc.communicate()
 					if proc.returncode != 0 :
+						errors += 1
+						messages.append(cmd)
 						print("ERROR: While runing {}\nDescription:\n{}".format(cmd, err))
-						sys.exit()
+
+print("INFO: Found {} errors in total.\nCommands failed:".format(errors))
+for m in messages :
+	print("-> {}".format(m))
+
+print("Total errors: {}".format(errors))
