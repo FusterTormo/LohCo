467f5e75abb32d015db638e7bf90ed5bb3480d2d
FusterTormo <franfustor@gmail.com>
2020-05-05 08:38:44 +0200

    Corregido error al calcular la VAF

--- reAnnoFilt.py
+++ reAnnoFilt.py
@@ -300,7 +300,7 @@ def main(ruta, samplename = "noName") :
         p["Strand_bias_score"] = calcularStrandBias(p)
         p["Ref_depth"] = p["AD"].split(",")[0]
         p["Alt_depth"] = p["AD"].split(",")[1]
-        p["VAF"] = float(p["Alt_depth"])/(p["DP"]) * 100
+        p["VAF"] = float(p["Alt_depth"])/float((p["DP"])) * 100
         p["IGV_link"] = "http://localhost:60151/goto?locus={}:{}".format(p["Chr"], p["Start"])
         p["sample"] = samplename
 
