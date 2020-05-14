9040aa0e46d3aeeb0f2fe622412785729c08ddf6
FusterTormo <franfustor@gmail.com>
2020-05-14 13:09:10 +0200

    Creada funcion que encuentra las parejas tumor-control

--- crearAnalisis.py
+++ crearAnalisis.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 
+import sys
+import subprocess
+
 #Librerias propias
 import getCommands
 
-def leerMuestras(ruta) :
+def leerMuestras() :
     """
-    Lee un archivo excel pasado por parametro, separa las muestas y crea la estructura
-    { "idMuestra" : {"tumor" : [ fw1.fastq, rv1.fastq, fw2.fastq, rv2.fastq ...], "control" : [ fw1.fastq, rv1.fastq ]}}
+    Lee los excels que contienen la informacion de las muestras. Separar las muestras con formato: {"id" : {"fastq" , [[fw1, rv1, rg1], [fw2, rv2, rg2], ...}, "gender" : "[M]/[F]"}
+    El diccionario de muestras control no tendra informacion del genero. Pero se tiene en cuenta que, de los controles generales, C2 es una mujer y C3 es un hombre
     """
-    #IMPORTANTE! C2 es un control de una mujer. C3 es un control de un hombre
     arx = ["MDS_12.csv", "MDS_13.csv", "MDS_14.csv", "MDS_15.csv"]
     paths = {"MDS_12.csv" : "../MDS_12/20190618/FASTQ", "MDS_13.csv" : "../MDS_13/20190612/FASTQ", "MDS_14.csv" : "../MDS_14/20200114/FASTQ", "MDS_15.csv" : "../MDS_15/20200114/FASTQ"}
     tumors = {}
@@ -37,7 +39,34 @@ def leerMuestras(ruta) :
                             tumors[id] = {"fastq" : [[fq1, fq2]], "gender" : aux[10]}
                         else :
                             tumors[id]["fastq"].append([fq1, fq2])
-    print("INFO: Trobats {} tumors i {} controls".format(len(tumors), len(controls)))
+    return tumors, controls
+
+def getControl(muestra, controles) :
+    # Extraer el numero identificador de la muestra tumoral
+    regex = "[\d]+"
+    match = re.search(regex, muestra)
+    idmuestra = ""
+    if match :
+        id = match.group()
+        print("INFO: Encontrado {} en la muestra {}".format(id, muestra))
+        for c in controles.keys() :
+            if c.endswith(id) :
+                idmuestra = c
+                break
+    else :
+        print("WARNING: Identificador no encontrado en {}".format(muestra))
+
+    return idmuestra
+
+
+def main() :
+    tm, cn = leerMuestras()
+    for k,v in tm.items() :
+        c = getControl(k, cn)
+        folder = "{}_VS_{}".format(t,c)
+        print(folder)
+    #alinear(tm)
+    #alinear(cn)
 
 if __name__ == "__main__" :
     leerMuestras()
