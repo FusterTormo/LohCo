c6b1d56c7a006c303793faaac476195acacfa85d
FusterTormo <franfustor@gmail.com>
2020-05-14 15:26:03 +0200

    Comandos para alinear las muestras tumorales

--- crearAnalisis.py
+++ crearAnalisis.py
@@ -3,9 +3,21 @@
 
 import sys
 import subprocess
+import re
 
 #Librerias propias
-import getCommands
+import getCommands as gc
+
+# Constantes
+hgref = ""
+snpSites = ""
+
+def crearRG(fq) :
+    """Crear el read group para el FASTQ pasado por parametro"""
+    aux = fq.split("_")
+    # El formato (arbitrario) del read group sera ID=flowcell, sample=index
+    rg = "\"@RG\\tID:{id}\\tSM:S{samp}\\tPL:ILLUMINA\"".format(id = aux[0], samp = aux[1])
+    return rg
 
 def leerMuestras() :
     """
@@ -29,16 +41,17 @@ def leerMuestras() :
                     fcell = aux[0].split("(")[0]
                     fq1 = "{dir}/{flowcell}_{lane}_{index}_1.fastq.gz".format(dir = dirpath, flowcell = fcell, lane = aux[1], index = aux[2])
                     fq2 = "{dir}/{flowcell}_{lane}_{index}_2.fastq.gz".format(dir = dirpath, flowcell = fcell, lane = aux[1], index = aux[2])
+                    rg = crearRG(fq1.split("/")[1])
                     if id.startswith("CD3") or id.endswith("CD3") : # El identificador indica que es un control
                         if id not in controls.keys() :
-                            controls[id] = {"fastq" : [[fq1, fq2]]}
+                            controls[id] = {"fastq" : [[fq1, fq2, rg]]}
                         else :
-                            controls[id]["fastq"].append([fq1, fq2])
+                            controls[id]["fastq"].append([fq1, fq2, rg])
                     else :
                         if id not in tumors.keys() :
-                            tumors[id] = {"fastq" : [[fq1, fq2]], "gender" : aux[10]}
+                            tumors[id] = {"fastq" : [[fq1, fq2, rg]], "gender" : aux[10]}
                         else :
-                            tumors[id]["fastq"].append([fq1, fq2])
+                            tumors[id]["fastq"].append([fq1, fq2, rg])
     return tumors, controls
 
 def getControl(muestra, controles) :
@@ -48,7 +61,6 @@ def getControl(muestra, controles) :
     idmuestra = ""
     if match :
         id = match.group()
-        print("INFO: Encontrado {} en la muestra {}".format(id, muestra))
         for c in controles.keys() :
             if c.endswith(id) :
                 idmuestra = c
@@ -63,10 +75,25 @@ def main() :
     tm, cn = leerMuestras()
     for k,v in tm.items() :
         c = getControl(k, cn)
-        folder = "{}_VS_{}".format(t,c)
-        print(folder)
-    #alinear(tm)
-    #alinear(cn)
+        if c == "" :
+            if v["gender"] == "F" :
+                c = "C2"
+            elif v["gender"] == "M" :
+                c = "C3"
+        if c != "" and c != "C2" and c != "C3" :
+            folder = "{}VS{}".format(t,c)
+            if os.path.isdir(folder) : # La carpeta existe, puede que algo se haya ejecutado
+                print("INFO: Comprobar que se ha hecho del analisis")
+            else :
+                print("INFO: Analizando {}".format(folder))
+                os.makedirs(folder, 0o754)
+                it = 1
+                for sample in v["fastq"] :
+                    print(gc.getAln(sample[2], hgref, sample[0], sample[1], "bwa_{}.sam".format(it)))
+                    it += 1
+                sys.exit()
+            #alinear(tm)
+            #alinear(cn)
 
 if __name__ == "__main__" :
-    leerMuestras()
+    main()
