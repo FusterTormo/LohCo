b4b2f09a3953d4b5a81550d69d4c2b5a24844497
FusterTormo <franfustor@gmail.com>
2020-05-18 13:14:19 +0200

    Added tool output filenames

--- testToolMatches.py
+++ testToolMatches.py
@@ -7,6 +7,14 @@ TEST TOOL MATCHES
     copy number divided by the total of regions that the case has. Value should be similar to accuracy in the confusion matrix
 """
 
+import os
+
+import main1 as mm
+
+# Constants
+dbcon = sqlite3.connect("/g/strcombio/fsupek_cancer2/TCGA_bam/info/info.db")
+wd = "/g/strcombio/fsupek_cancer2/TCGA_bam/OV"
+
 with dbcon :
     cur = dbcon.cursor()
     q = cur.execute("SELECT submitter FROM patient WHERE cancer='OV'")
@@ -24,5 +32,16 @@ for c in cases :
         for cn in controls :
             # Get the absolute path the and the prefix for the tool output
             tf = "{wd}/{sub}/{tm}_VS_{cn}".format(wd = wd, sub = c[0], tm = tm[0].split("-")[0], cn = cn[0].split("-")[0])
+            facets = "{}_FACETS/facets_comp_cncf.tsv".format(tf)
+            ascat = mm.findAscatName("{}_ASCAT/".format(tf))
+            sequenza = "{}_Sequenza/{}_segments.txt".format(tf, c[0])
+            if os.path.isfile(facets) and os.path.isfile(ascat) :
+                print("Comparar {} amb {}".format(facets, ascat))
+            if os.path.isfile(facets) and os.path.isfile(sequenza) :
+                print("Comparar {} amb {}".format(facets, sequenza))
+            if os.path.isfile(ascat) and os.path.isfile(sequenza) :
+                print("Comparar {} amb {}".format(ascat, sequenza))
+            if os.path.isfile(facets) and os.path.isfile(ascat) and os.path.isfile(sequenza) :
+                print("Comparar totes les eines")
             print(tf)
             sys.exit()
