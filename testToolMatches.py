79a3fb676c6e8bd58ed172e60a48c236bf3b992b
FusterTormo <franfustor@gmail.com>
2020-05-22 13:36:40 +0200

    Added other correlation and similarity tests to the analisys

--- testToolMatches.py
+++ testToolMatches.py
@@ -5,6 +5,9 @@
 TEST TOOL MATCHES
     For each sample, calculates the percent of similarity. Similarity is defined as the number of regions where two (or three) tools report the same
     copy number divided by the total of regions that the case has. Value should be similar to accuracy in the confusion matrix
+    Additionally, calculates the MCC, and the Jaccard index for all the aberrations
+    Finally it calculates the percentage of coincidences by inspecting the regions in common
+    The output is printed in separated files one for each comparison
 """
 
 import os
@@ -14,10 +17,24 @@ import main1 as mm
 import libcomparison as lc
 import libstatistics as ls
 import libconstants as cte
+import libgetters as lg
 
 # Constants
 dbcon = sqlite3.connect("/g/strcombio/fsupek_cancer2/TCGA_bam/info/info.db")
 wd = "/g/strcombio/fsupek_cancer2/TCGA_bam/OV"
+
+def percentSimilarity(regs, tool1, tool2) :
+    coin = 0
+    all = 0
+    for k in regs.keys() :
+        for r in regs[k] :
+            all += 1
+            if lg.getCopyNumber(r, k, tool1) == lg.getCopyNumber(r, k, tool2) :
+                coin += 1
+
+    percent = 100*float(coin)/float(all)
+    return percent
+
 
 def calculateSimilarity(dc) :
     dividendo = 0
@@ -30,6 +47,17 @@ def calculateSimilarity(dc) :
     return similarity
 
 def main () :
+    fvaFi = "facetsVSascatngs.tsv"
+    fvsFi = "facetsVSsequenza.tsv"
+    avsFi = "ascatVSsequenza.tsv"
+    # Write the output files' header
+    with open(fvaFI, "w") as fi :
+        fi.write("Case\tpercent\tACC\tMCCA\tMCCN\tMCCL\tMCCD\tjcca\tjccn\tjccl\tjccd\n")
+    with open(fvsFI, "w") as fi :
+        fi.write("Case\tpercent\tACC\tMCCA\tMCCN\tMCCL\tMCCD\tjcca\tjccn\tjccl\tjccd\n")
+    with open(avsFI, "w") as fi :
+        fi.write("Case\tpercent\tACC\tMCCA\tMCCN\tMCCL\tMCCD\tjcca\tjccn\tjccl\tjccd\n")
+
     table = []
     with dbcon :
         cur = dbcon.cursor()
@@ -46,10 +74,14 @@ def main () :
             controls = q.fetchall()
         for tm in tumors :
             for cn in controls :
-                aux = []
+                fva = []
+                fvs = []
+                avs = []
                 # Get the absolute path the and the prefix for the tool output
                 tf = "{wd}/{sub}/{tm}_VS_{cn}".format(wd = wd, sub = c[0], tm = tm[0].split("-")[0], cn = cn[0].split("-")[0])
-                aux.append(tf)
+                fva.append(tf)
+                fvs.append(tf)
+                avs.append(tf)
                 facets = "{}_FACETS/facets_comp_cncf.tsv".format(tf)
                 ascat = mm.findAscatName("{}_ASCAT/".format(tf))
                 sequenza = "{}_Sequenza/{}_segments.txt".format(tf, c[0])
@@ -59,33 +91,77 @@ def main () :
                     outa = lc.convert2region(ascat, "ascatngs")
                 if os.path.isfile(sequenza) :
                     outs = lc.convert2region(sequenza, "sequenza")
-
+                # Compare FACETS vs ascatNGS
                 if os.path.isfile(facets) and os.path.isfile(ascat) :
                     regs = lc.getFragments(outf, outa)
-                    dc = lc.doComparison2(regs, outf, outa)
-                    # print(ls.printTable(dc, "FACETS", "ascatNGS", False))
-                    aux.append(calculateSimilarity(dc))
+                    c1 = lc.doComparison(regs, outf, outa)
+                    c2 = lc.doComparison2(regs, outf, outa)
+                    sts = ls.doContingency(c1) # Get the MCC for all the aberrations
+                    jcc = ls.jaccardIndex(c2)
+                    fva.append(calculateSimilarity(c2))
+                    fva.append(percentSimilarity(regs, outf, outa))
+                    for ab in cte.aberrations :
+                        fva.append(sts[ab]["MCC"])
+                    for ab in cte.aberrations :
+                        fva.append(jcc[ab])
                 else :
-                    aux.append("NA")
+                    fva.append("NA")
+                    fva.append("NA")
+                    for ab in cte.aberrations :
+                        fva.append("NA")
+                    for ab in cte.aberrations :
+                        fva.append("NA")
+                # Compare FACETS VS Sequenza
                 if os.path.isfile(facets) and os.path.isfile(sequenza) :
                     regs = lc.getFragments(outf, outs)
-                    dc = lc.doComparison2(regs, outf, outs)
-                    # print(ls.printTable(dc, "FACETS", "Sequenza", False))
-                    aux.append(calculateSimilarity(dc))
+                    c1 = lc.doComparison(regs, outf, outs)
+                    c2 = lc.doComparison2(regs, outf, outs)
+                    sts = ls.doContingency(c1) # Get the MCC for all the aberrations
+                    jcc = ls.jaccardIndex(c2) # Get the Jaccard index for all the aberrations
+                    fvs.append(calculateSimilarity(c2))
+                    fvs.append(percentSimilarity(regs, outf, outs))
+                    for ab in cte.aberrations :
+                        fvs.append(sts[ab]["MCC"])
+                    for ab in cte.aberrations :
+                        fvs.append(jcc[ab])
                 else :
-                    aux.append("NA")
+                    fvs.append("NA")
+                    fvs.append("NA")
+                    for ab in cte.aberrations :
+                        fvs.append("NA")
+                    for ab in cte.aberrations :
+                        fvs.append("NA")
+                # Compare ascatNGS VS Sequenza
                 if os.path.isfile(ascat) and os.path.isfile(sequenza) :
                     regs = lc.getFragments(outa, outs)
-                    dc = lc.doComparison2(regs, outa, outs)
-                    # print(ls.printTable(dc, "ascatNGS", "Sequenza", False))
-                    aux.append(calculateSimilarity(dc))
+                    c1 = lc.doComparison(regs, outa, outs)
+                    c2 = lc.doComparison2(regs, outa, outs)
+                    sts = ls.doContingency(c1) # Get the MCC for all the aberrations
+                    jcc = ls.jaccardIndex(c2) # Get the Jaccard index for all the aberrations
+                    fvs.append(calculateSimilarity(c2))
+                    fvs.append(percentSimilarity(regs, outa, outs))
+                    for ab in cte.aberrations :
+                        avs.append(sts[ab]["MCC"])
+                    for ab in cte.aberrations :
+                        avs.append(jcc[ab])
                 else :
-                    aux.append("NA")
-                table.append(aux)
-    print("INFO: Printing the data in a table")
-    with open("testToolSimilarity.txt", "w") as fi :
-        fi.write("uuid\tfva\tfvs\tavs\n")
-        for t in table :
-            fi.write("{}\t{}\t{}\t{}\n".format(t[0], t[1], t[2], t[3]))
+                    fvs.append("NA")
+                    fvs.append("NA")
+                    for ab in cte.aberrations :
+                        avs.append("NA")
+                    for ab in cte.aberrations :
+                        avs.append("NA")
+                # Write the output in the corresponding files for each comparison
+                with open(fvaFI, "a") as fi :
+                    fi.write("\t".join(fva))
+                    fi.write("\n")
+                with open(fvsFI, "a") as fi :
+                    fi.write("\t".join(fvs))
+                    fi.write("\n")
+                with open(avsFI, "a") as fi :
+                    fi.write("\t".join(avs))
+                    fi.write("\n")
+
+
 
 main()
