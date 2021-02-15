35232a0303e236afb8bd8c54e98ed7d3db60e781
FusterTormo <franfustor@gmail.com>
2021-02-15 09:53:26 +0100

    Added flexibility to test the LOH in different genes

--- newMain7.py
+++ newMain7.py
@@ -127,150 +127,154 @@ def doLoh(path, region) :
 
     return (program, loh)
 
+
 # Main program
+def main(brcagene, genename) :
+    totalPos = 0
+    dcPos = {"ascat2" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0, "NF" : 0}, "facets" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0},
+    "ascatngs" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}, "sequenza" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}}
+    totalNeg = 0
+    dcNeg = {"ascat2" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0, "NF" : 0}, "facets" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0},
+    "ascatngs" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}, "sequenza" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}}
+    totalNeu = 0
+    dcNeu = {"ascat2" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0, "NF" : 0}, "facets" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0},
+    "ascatngs" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}, "sequenza" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}}
+    # Get submitters list
+    with dbcon :
+          cur = dbcon.cursor()
+          q = cur.execute("SELECT submitter FROM patient WHERE cancer='OV'")
+          cases = q.fetchall()
+
+    print("INFO: Analysis done in {} cases".format(len(cases)))
+    for c in cases :
+        if (totalPos + totalNeg + totalNeu) % 100 == 0 :
+            print("{} cases analysed".format(totalPos + totalNeg + totalNeu))
+
+        with dbcon :
+            cur = dbcon.cursor()
+            q = cur.execute("SELECT uuid, bamName FROM sample WHERE submitter='{}' AND tumor LIKE '%Tumor%'".format(c[0]))
+            tumors = q.fetchall()
+            q = cur.execute("SELECT uuid, bamName FROM sample WHERE submitter='{}' AND tumor LIKE '%Normal%'".format(c[0]))
+            controls = q.fetchall()
+
+        # Get the pair tumor/control with more files
+        submitter = {"tumor" : "", "control" : "", "vcfFiles" : [], "lohFiles" : []}
+        for tm in tumors :
+            for cn in controls :
+                auxDc = {"tumor" : tm[0], "control" : cn[0], "vcfFiles" : [], "lohFiles" : []}
+                tf = "{wd}/{sub}/{tumor}".format(wd = wd, sub = c[0], tumor = tm[0])
+                cf = "{wd}/{sub}/{control}".format(wd = wd, sub = c[0], control = cn[0])
+                workindir = "{wd}/{sub}".format(wd = wd, sub = c[0])
+                analysisdir = "{}_VS_{}".format(tm[0].split("-")[0], cn[0].split("-")[0]) # The folder format for FACETS, ascatNGS, and Sequenza is "[tumorUUID]_VS_[controlUUID]""
+                vct = "{}/platypusGerm/platypus.hg38_multianno.txt".format(tf)
+                vcc = "{}/platypusGerm/platypus.hg38_multianno.txt".format(cf)
+                # Check both variant calling files exist
+                if os.path.isfile(vct) :
+                    auxDc["vcfFiles"].append(vct)
+                if os.path.isfile(vcc) :
+                    auxDc["vcfFiles"].append(vcc)
+                folder = "{}/ASCAT2".format(workindir)
+                if os.path.isdir(folder) and len(os.listdir(folder)) > 0:
+                    auxDc["lohFiles"].append(folder)
+                facets = "{wd}/{folder}_FACETS/facets_comp_cncf.tsv".format(wd = workindir, folder = analysisdir)
+                if os.path.isfile(facets) :
+                    auxDc["lohFiles"].append(facets)
+                ascatngs = lib.findAscatName("{wd}/{folder}_ASCAT/".format(wd = workindir, folder = analysisdir))
+                if ascatngs != "Not found" :
+                    auxDc["lohFiles"].append(ascatngs)
+                sequenza = "{wd}/{folder}_Sequenza/{case}_segments.txt".format(folder = analysisdir, case = c[0], wd = workindir)
+                if os.path.isfile(sequenza) :
+                    auxDc["lohFiles"].append(sequenza)
+                if len(auxDc["vcfFiles"]) > len(submitter["vcfFiles"]) and len(auxDc["lohFiles"]) > len(submitter["vcfFiles"]) :
+                    submitter = auxDc.copy()
+
+        # Get the variants in BRCA1 and BRCA2 genes
+        if len(submitter["vcfFiles"]) == 2 :
+            gmmaf, gmnoMaf = getVariant(submitter["vcfFiles"][1], genename)
+            # IDEA: Podria fer multiprocessing en aquesta busqueda
+            #tmmaf, tmnoMaf = getVariant(submitter["vcfFiles"][0], "BRCA1")
+            # Classify the variants according to the pathogenicity
+            varClass = classifyVariants(gmmaf, gmnoMaf)
+            # Get LOH in the region
+            try :
+                prog1, loh1 = doLoh(submitter["lohFiles"][0], brcagene)
+                if varClass == "+" :
+                    dcPos[prog1][loh1] += 1
+                elif varClass == "-" :
+                    dcNeg[prog1][loh1] += 1
+                elif varClass == "?" :
+                    dcNeu[prog1][loh1] += 1
+            except IndexError :
+                prog1 = None
+                loh1 = None
+            try :
+                prog2, loh2 = doLoh(submitter["lohFiles"][1], brcagene)
+                if varClass == "+" :
+                    dcPos[prog2][loh2] += 1
+                elif varClass == "-" :
+                    dcNeg[prog2][loh2] += 1
+                elif varClass == "?" :
+                    dcNeu[prog2][loh2] += 1
+            except IndexError :
+                prog2 = None
+                loh2 = None
+            try :
+                prog3, loh3 = doLoh(submitter["lohFiles"][2], brcagene)
+                if varClass == "+" :
+                    dcPos[prog3][loh3] += 1
+                elif varClass == "-" :
+                    dcNeg[prog3][loh3] += 1
+                elif varClass == "?" :
+                    dcNeu[prog3][loh3] += 1
+            except IndexError :
+                prog3 = None
+                loh3 = None
+            try :
+                prog4, loh4 = doLoh(submitter["lohFiles"][3], brcagene)
+                if varClass == "+" :
+                    dcPos[prog4][loh4] += 1
+                elif varClass == "-" :
+                    dcNeg[prog4][loh4] += 1
+                elif varClass == "?" :
+                    dcNeu[prog4][loh4] += 1
+            except IndexError :
+                prog4 = None
+                loh4 = None
+            # Count the number of each
+            if varClass == "+" :
+                totalPos += 1
+            elif varClass == "-" :
+                totalNeg += 1
+            elif varClass == "?" :
+                totalNeu += 1
+            # # # IDEA: Podria fer multiprocessing en la busqueda de cada arxiu LOH
+            # print("{} -> {} - {}".format(submitter["lohFiles"][0], prog1, loh1))
+            # print("{} -> {} - {}".format(submitter["lohFiles"][1], prog2, loh2))
+            # print("{} -> {} - {}".format(submitter["lohFiles"][2], prog3, loh3))
+            # print(dcPos)
+
+    print("INFO: Final results\n")
+    print("{} cases considered positive in BRCA1".format(totalPos))
+    for k in dcPos.keys() :
+        print("\t{} ({} analyses)".format(k, sum(dcPos[k].values())))
+        for key, value in dcPos[k].items() :
+            print("\t\t{} -> {} found".format(key, value))
+        print("\t\t\t{:.2f}% LOH".format(100 * (dcPos[k]["D"] + dcPos[k]["L"])/totalPos))
+
+    print("\n{} cases considered negative in BRCA1".format(totalNeg))
+    for k in dcNeg.keys() :
+        print("\t{} ({} analyses)".format(k, sum(dcPos[k].values())))
+        for key, value in dcNeg[k].items() :
+            print("\t\t{} -> {} found".format(key, value))
+        print("\t\t\t{:.2f}% LOH".format(100 * (dcNeg[k]["D"] + dcNeg[k]["L"])/totalPos))
+
+    print("\n{} cases considered unknown in BRCA1".format(totalNeu))
+    for k in dcNeu.keys() :
+        print("\t{} ({} analyses)".format(k, sum(dcPos[k].values())))
+        for key, value in dcNeu[k].items() :
+            print("\t\t{} -> {} found".format(key, value))
+        print("\t\t\t{:.2f}% LOH".format(100 * (dcNeu[k]["D"] + dcNeu[k]["L"])/totalPos))
+
 brca1 = ["17", 43044295, 43170245]
 brca2 = ["13", 32315086, 32400266]
-totalPos = 0
-dcPos = {"ascat2" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0, "NF" : 0}, "facets" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0},
-"ascatngs" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}, "sequenza" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}}
-totalNeg = 0
-dcNeg = {"ascat2" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0, "NF" : 0}, "facets" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0},
-"ascatngs" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}, "sequenza" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}}
-totalNeu = 0
-dcNeu = {"ascat2" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0, "NF" : 0}, "facets" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0},
-"ascatngs" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}, "sequenza" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}}
-# Get submitters list
-with dbcon :
-      cur = dbcon.cursor()
-      q = cur.execute("SELECT submitter FROM patient WHERE cancer='OV'")
-      cases = q.fetchall()
-
-print("INFO: Analysis done in {} cases".format(len(cases)))
-for c in cases :
-    if (totalPos + totalNeg + totalNeu) % 100 == 0 :
-        print("{} cases analysed".format(totalPos + totalNeg + totalNeu))
-
-    with dbcon :
-        cur = dbcon.cursor()
-        q = cur.execute("SELECT uuid, bamName FROM sample WHERE submitter='{}' AND tumor LIKE '%Tumor%'".format(c[0]))
-        tumors = q.fetchall()
-        q = cur.execute("SELECT uuid, bamName FROM sample WHERE submitter='{}' AND tumor LIKE '%Normal%'".format(c[0]))
-        controls = q.fetchall()
-
-    # Get the pair tumor/control with more files
-    submitter = {"tumor" : "", "control" : "", "vcfFiles" : [], "lohFiles" : []}
-    for tm in tumors :
-        for cn in controls :
-            auxDc = {"tumor" : tm[0], "control" : cn[0], "vcfFiles" : [], "lohFiles" : []}
-            tf = "{wd}/{sub}/{tumor}".format(wd = wd, sub = c[0], tumor = tm[0])
-            cf = "{wd}/{sub}/{control}".format(wd = wd, sub = c[0], control = cn[0])
-            workindir = "{wd}/{sub}".format(wd = wd, sub = c[0])
-            analysisdir = "{}_VS_{}".format(tm[0].split("-")[0], cn[0].split("-")[0]) # The folder format for FACETS, ascatNGS, and Sequenza is "[tumorUUID]_VS_[controlUUID]""
-            vct = "{}/platypusGerm/platypus.hg38_multianno.txt".format(tf)
-            vcc = "{}/platypusGerm/platypus.hg38_multianno.txt".format(cf)
-            # Check both variant calling files exist
-            if os.path.isfile(vct) :
-                auxDc["vcfFiles"].append(vct)
-            if os.path.isfile(vcc) :
-                auxDc["vcfFiles"].append(vcc)
-            folder = "{}/ASCAT2".format(workindir)
-            if os.path.isdir(folder) and len(os.listdir(folder)) > 0:
-                auxDc["lohFiles"].append(folder)
-            facets = "{wd}/{folder}_FACETS/facets_comp_cncf.tsv".format(wd = workindir, folder = analysisdir)
-            if os.path.isfile(facets) :
-                auxDc["lohFiles"].append(facets)
-            ascatngs = lib.findAscatName("{wd}/{folder}_ASCAT/".format(wd = workindir, folder = analysisdir))
-            if ascatngs != "Not found" :
-                auxDc["lohFiles"].append(ascatngs)
-            sequenza = "{wd}/{folder}_Sequenza/{case}_segments.txt".format(folder = analysisdir, case = c[0], wd = workindir)
-            if os.path.isfile(sequenza) :
-                auxDc["lohFiles"].append(sequenza)
-            if len(auxDc["vcfFiles"]) > len(submitter["vcfFiles"]) and len(auxDc["lohFiles"]) > len(submitter["vcfFiles"]) :
-                submitter = auxDc.copy()
-
-    # Get the variants in BRCA1 and BRCA2 genes
-    if len(submitter["vcfFiles"]) == 2 :
-        gmmaf, gmnoMaf = getVariant(submitter["vcfFiles"][1], "BRCA1")
-        # IDEA: Podria fer multiprocessing en aquesta busqueda
-        #tmmaf, tmnoMaf = getVariant(submitter["vcfFiles"][0], "BRCA1")
-        # Classify the variants according to the pathogenicity
-        varClass = classifyVariants(gmmaf, gmnoMaf)
-        # Get LOH in the region
-        try :
-            prog1, loh1 = doLoh(submitter["lohFiles"][0], brca1)
-            if varClass == "+" :
-                dcPos[prog1][loh1] += 1
-            elif varClass == "-" :
-                dcNeg[prog1][loh1] += 1
-            elif varClass == "?" :
-                dcNeu[prog1][loh1] += 1
-        except IndexError :
-            prog1 = None
-            loh1 = None
-        try :
-            prog2, loh2 = doLoh(submitter["lohFiles"][1], brca1)
-            if varClass == "+" :
-                dcPos[prog2][loh2] += 1
-            elif varClass == "-" :
-                dcNeg[prog2][loh2] += 1
-            elif varClass == "?" :
-                dcNeu[prog2][loh2] += 1
-        except IndexError :
-            prog2 = None
-            loh2 = None
-        try :
-            prog3, loh3 = doLoh(submitter["lohFiles"][2], brca1)
-            if varClass == "+" :
-                dcPos[prog3][loh3] += 1
-            elif varClass == "-" :
-                dcNeg[prog3][loh3] += 1
-            elif varClass == "?" :
-                dcNeu[prog3][loh3] += 1
-        except IndexError :
-            prog3 = None
-            loh3 = None
-        try :
-            prog4, loh4 = doLoh(submitter["lohFiles"][3], brca1)
-            if varClass == "+" :
-                dcPos[prog4][loh4] += 1
-            elif varClass == "-" :
-                dcNeg[prog4][loh4] += 1
-            elif varClass == "?" :
-                dcNeu[prog4][loh4] += 1
-        except IndexError :
-            prog4 = None
-            loh4 = None
-        # Count the number of each
-        if varClass == "+" :
-            totalPos += 1
-        elif varClass == "-" :
-            totalNeg += 1
-        elif varClass == "?" :
-            totalNeu += 1
-        # # # IDEA: Podria fer multiprocessing en la busqueda de cada arxiu LOH
-        # print("{} -> {} - {}".format(submitter["lohFiles"][0], prog1, loh1))
-        # print("{} -> {} - {}".format(submitter["lohFiles"][1], prog2, loh2))
-        # print("{} -> {} - {}".format(submitter["lohFiles"][2], prog3, loh3))
-        # print(dcPos)
-
-print("INFO: Final results\n")
-print("{} cases considered positive in BRCA1".format(totalPos))
-for k in dcPos.keys() :
-    print("\t{} ({} analyses)".format(k, sum(dcPos[k].values())))
-    for key, value in dcPos[k].items() :
-        print("\t\t{} -> {} found".format(key, value))
-    print("\t\t\t{:.2f}% LOH".format(100 * (dcPos[k]["D"] + dcPos[k]["L"])/totalPos))
-
-print("\n{} cases considered negative in BRCA1".format(totalNeg))
-for k in dcNeg.keys() :
-    print("\t{}".format(k))
-    for key, value in dcNeg[k].items() :
-        print("\t\t{} -> {} found".format(key, value))
-    print("\t\t\t{:.2f}% LOH".format(100 * (dcNeg[k]["D"] + dcNeg[k]["L"])/totalPos))
-
-print("\n{} cases considered unknown in BRCA1".format(totalNeu))
-for k in dcNeu.keys() :
-    print("\t{}".format(k))
-    for key, value in dcNeu[k].items() :
-        print("\t\t{} -> {} found".format(key, value))
-    print("\t\t\t{:.2f}% LOH".format(100 * (dcNeu[k]["D"] + dcNeu[k]["L"])/totalPos))
+main(brca1, "BRCA1")
