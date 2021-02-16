ee4e31679b4a4a6805fff0a010f53b35d3b7aebe
FusterTormo <franfustor@gmail.com>
2021-02-16 10:43:19 +0100

    Removed old main7
    Replaced by newMain7

--- main7.py
+++ main7.py
@@ -1,96 +1,153 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-"""
-MAIN: Check the output by 5 LOH tools (ASCAT2, DNAcopy, FACETS, ascatNGS, and Sequenza) in the genes of interest (BRCA1 and BRCA2)
-Assumes that if there is a deleterious mutation in the gene (BRCA1 for example) there will be an LOH in the other gene copy (Deletion or copy number neutral LOH)
-Compares how many coincidences each tool reports. A coincidence is when there is the deleterious mutation and the LOH in the gene.
-"""
-
-# Libraries
 import os
 import sqlite3
+import subprocess
 import sys
 
-import libgetters as lg
-import libcomparison as lc
 import main1 as lib
-import libconstants as cts
+import libconstants as ctes
 
 # Constants
 dbcon = sqlite3.connect("/g/strcombio/fsupek_cancer2/TCGA_bam/info/info.db")
 wd = "/g/strcombio/fsupek_cancer2/TCGA_bam/OV"
 
-def getMutation(path, gene) :
-    """Classify the worst mutation in the gene
+# Functions
+def getMaxMaf(ls) :
+    maf = -1
+    for l in ls :
+        try :
+            aux = float(l)
+            if aux >= maf :
+                maf = aux
+        except ValueError :
+            pass
+    if maf == -1 :
+        maf = "NA"
 
-    Given a list of whole exome mutations (path to vcf file), extracts the mutation in the gene, and gets the considered the worst mutation. If this worst mutation is considered deleterious
-    returns '+'; if the mutation is considered tolerated returns '-'; in other case the mutation is considered unknown and returns '?'
+    return maf
 
-    Parameters
-    ----------
-        path : str
-            Path to the vcf file that contains all the mutation
-        gene : str
-            Gene of interest to extract the worst mutation
+def getVariant(path, gene) :
+    removableVars = ["intergenic", "intronic", "unkwnonw", "downstream", "upstream"]
+    noMaf = []
+    wMaf = {"varType1" : "", "varType2" : "", "maf" : 2}
 
-    Returns
-    -------
-        str
-            * '+' if the worst mutation is considered deleterious ("nonframeshift deletion", "nonframeshift insertion", "frameshift substitution", "frameshift block substitution", "frameshift deletion",
-            "frameshift insertion", "stopgain", "splicing")
-            * '-' if the worst mutation is considered tolerated ("NA", "synonymous SNV")
-            * '?' if the worst mutation is considered unknown ("nonsynonymous SNV", "nonframeshift substitution", "nonframeshift block substitution", "stoploss")
-    """
-    mut = lib.getWorst(path, gene)
-    clas = "-"
-    if mut in cts.var_positive :
-        clas = "+"
-    elif mut in cts.var_negative :
-        clas = "-"
-    elif mut in cts.var_neutral :
-        clas =  "?"
-    else :
-        clas = "NF"
-
-    return clas
-
-def checkAscat(folder, reg) :
-    ascat = "{}/ASCAT2".format(folder)
-    cn = "NF"
-    if os.path.isdir(ascat) :
-        files = os.listdir(ascat)
-        if len(files) == 1 :
-            abs = "{}/{}".format(ascat, files[0])
-            cn = lib.getLOH(abs, "ascatarray", reg)
-        else :
-            for f in files :
-                abs = "{}/{}".format(ascat, f)
-                if cn == "" :
-                    cn = lib.getLOH(abs, "ascatarray", reg)
+    # Find the variants called in BRCA1
+    cmd = "grep {gene} {vc}".format(vc = path, gene = gene)
+    pr = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    std, err = pr.communicate()
+    out = std.decode().strip().split("\n")
+    for tmp in out :
+        aux = tmp.split("\t")
+        try :
+            varType = aux[5]
+            varType2 = aux[8]
+            if varType not in removableVars :
+                maf = getMaxMaf(aux[10:39])
+                if maf == "NA" :
+                    noMaf.append({"varType1" : varType, "varType2" : varType2, "maf" : maf, "GT": aux[-1]})
                 else :
-                    auxCn = lib.getLOH(abs, "ascatarray", reg)
-                    if cn != auxCn : # If the ASCAT2 outpus does not output the same aberration, we do not include the result
-                        cn =  "NF"
-                        break
+                    if maf < wMaf["maf"] :
+                        wMaf["varType1"] = varType
+                        wMaf["varType2"] = varType2
+                        wMaf["maf"] = maf
+                        wMaf["GT"] = aux[-1]
+        except IndexError :
+            pass
+
+    if wMaf["maf"] < 2 :
+        return (wMaf, noMaf)
+    else :
+        return (None, noMaf)
+
+
+def classifyVariants(maf, noMaf, maxMaf) :
+    classification = "-"
+    if maf is not None :
+        if maf["varType1"] == "exonic" :
+            if maf["varType2"] in ctes.var_negative :
+                classification = "-"
+            elif maf["varType2"] in ctes.var_positive :
+                classification = "+"
+            else :
+                if maf["maf"] <= maxMaf :
+                    classification = "+"
+                else :
+                    classification = "?"
+        elif maf["varType1"] == "splicing" :
+            classification = "+"
+
+    if classification == "-" or classification == "?" :
+        for v in noMaf :
+            if v["varType1"] == "exonic" :
+                if v["varType2"] in ctes.var_neutral or v["varType2"] in ctes.var_positive :
+                    classification = "+"
+                    break
+            elif v["varType1"] == "splicing" :
+                classification = "+"
+                break
+    return classification
+
+def checkAscat(ascat, reg) :
+    cn = "NF"
+    files = os.listdir(ascat)
+    if len(files) == 1 :
+        abs = "{}/{}".format(ascat, files[0])
+        cn = lib.getLOH(abs, "ascatarray", reg)
+    else :
+        for f in files :
+            abs = "{}/{}".format(ascat, f)
+            if cn == "" :
+                cn = lib.getLOH(abs, "ascatarray", reg)
+            else :
+                auxCn = lib.getLOH(abs, "ascatarray", reg)
+                if cn != auxCn : # If the ASCAT2 outpus does not output the same aberration, we do not include the result
+                    cn =  "NF"
+                    break
     return cn
 
+def doLoh(path, region) :
+    program = ""
+    loh = ""
+    # Get from which program is the output
+    if path.endswith("facets_comp_cncf.tsv") :
+        program = "facets"
+    elif path.endswith("copynumber.caveman.csv") :
+        program = "ascatngs"
+    elif path.endswith("_segments.txt") :
+        program = "sequenza"
+    else :
+        program = "ascat2"
+    if program == "ascat2" :
+        loh = checkAscat(path, region)
+    else :
+        loh = lib.getLOH(path, program, region)
 
-def doTest(gene, region) :
-    output = "ID1\tID2\tgerm\tsom\tASCAT\tFACETS\tascatNGS\tSequenza\n"
-    cont = 0
-    # Get the OV cases
+    return (program, loh)
+
+
+# Main program
+def main(brcagene, genename, vcPath, maxMaf = 0.01) :
+    totalPos = 0
+    dcPos = {"ascat2" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0, "NF" : 0}, "facets" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0},
+    "ascatngs" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}, "sequenza" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}}
+    totalNeg = 0
+    dcNeg = {"ascat2" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0, "NF" : 0}, "facets" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0},
+    "ascatngs" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}, "sequenza" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}}
+    totalNeu = 0
+    dcNeu = {"ascat2" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0, "NF" : 0}, "facets" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0},
+    "ascatngs" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}, "sequenza" : {"L" : 0, "A" : 0, "D" : 0, "N" : 0}}
+    # Get submitters list
     with dbcon :
-        cur = dbcon.cursor()
-        q = cur.execute("SELECT submitter FROM patient WHERE cancer='OV'")
-        cases = q.fetchall()
+          cur = dbcon.cursor()
+          q = cur.execute("SELECT submitter FROM patient WHERE cancer='OV'")
+          cases = q.fetchall()
 
-    print("INFO: {} cases found".format(len(cases)))
-    # Get bam information from each case and to do the
+    print("INFO: Analysis done in {} cases".format(len(cases)))
     for c in cases :
-        cont += 1
-        if cont % 100 == 0 :
-            print("INFO: {} cases checked".format(cont))
+        if (totalPos + totalNeg + totalNeu) % 100 == 0 :
+            print("{} cases analysed".format(totalPos + totalNeg + totalNeu))
 
         with dbcon :
             cur = dbcon.cursor()
@@ -99,57 +156,131 @@ def doTest(gene, region) :
             q = cur.execute("SELECT uuid, bamName FROM sample WHERE submitter='{}' AND tumor LIKE '%Normal%'".format(c[0]))
             controls = q.fetchall()
 
+        # Get the pair tumor/control with more files
+        submitter = {"tumor" : "", "control" : "", "vcfFiles" : [], "lohFiles" : []}
         for tm in tumors :
             for cn in controls :
-                # Get the absolute path for the platypus hg38_multianno file in tumor and control
+                auxDc = {"tumor" : tm[0], "control" : cn[0], "vcfFiles" : [], "lohFiles" : []}
                 tf = "{wd}/{sub}/{tumor}".format(wd = wd, sub = c[0], tumor = tm[0])
                 cf = "{wd}/{sub}/{control}".format(wd = wd, sub = c[0], control = cn[0])
                 workindir = "{wd}/{sub}".format(wd = wd, sub = c[0])
-                analysisdir = "{}_VS_{}".format(tm[0].split("-")[0], cn[0].split("-")[0]) # The folder format for FACETS, ascatNGS, and Sequenza is "[tumorUUID]_VS_[controlUUID]"
-                # Variant calling paths for Platypus
-                # platypust = "{}/platypusGerm/platypus.hg38_multianno.txt".format(tf)
-                # platypusc = "{}/platypusGerm/platypus.hg38_multianno.txt".format(cf)
-                # Variant calling paths for Strelka2
-                platypust = "{}/strelkaGerm/results/variants/strelka.hg38_multianno.txt".format(tf)
-                platypusc = "{}/strelkaGerm/results/variants/strelka.hg38_multianno.txt".format(cf)
-                if os.path.isfile(platypusc) and os.path.isfile(platypust) :
-                    # Get the information regarding the worst variant in the gene selected found in platypus variant calling
-                    mut_cn = getMutation(platypusc, gene)
-                    mut_sm = getMutation(platypust, gene)
-                    # Get the copy number output from ASCAT2
-                    asc = checkAscat(workindir, region)
-                    # Get the copy number output from FACETS
-                    path = "{wd}/{folder}_FACETS/facets_comp_cncf.tsv".format(wd = workindir, folder = analysisdir)
-                    fac = lib.getLOH(path, "facets", region)
-                    # Get the copy number output from ascatNGS
-                    if fac == "Not found" :
-                        fac = "NF"
-                    path = lib.findAscatName("{wd}/{folder}_ASCAT/".format(wd = workindir, folder = analysisdir))
-                    ngs = lib.getLOH(path, "ascatngs", region)
-                    if ngs == "Not found" :
-                        ngs = "NF"
-                    # Get the copy number output from Sequenza
-                    path = "{wd}/{folder}_Sequenza/{case}_segments.txt".format(folder = analysisdir, case = c[0], wd = workindir)
-                    seq = lib.getLOH(path, "sequenza", region)
-                    if seq == "Not found" :
-                        seq = "NF"
-                    output += "{id1}\t{id2}\t{mtcn}\t{mtsm}\t{cnasc}\t{cnfac}\t{cnngs}\t{cnseq}\n".format(id1 = tm[0], id2 = cn[0], mtcn = mut_cn, mtsm = mut_sm, cnasc = asc, cnfac = fac, cnngs= ngs, cnseq = seq)
-                else :
-                    print("WARNING: Case {} unable to analyse".format(analysisdir))
-    return output
+                analysisdir = "{}_VS_{}".format(tm[0].split("-")[0], cn[0].split("-")[0]) # The folder format for FACETS, ascatNGS, and Sequenza is "[tumorUUID]_VS_[controlUUID]""
+                vct = vcPath.format(tf)
+                vcc = vcPath.format(cf)
+                # Check both variant calling files exist
+                if os.path.isfile(vct) :
+                    auxDc["vcfFiles"].append(vct)
+                if os.path.isfile(vcc) :
+                    auxDc["vcfFiles"].append(vcc)
+                folder = "{}/ASCAT2".format(workindir)
+                if os.path.isdir(folder) and len(os.listdir(folder)) > 0:
+                    auxDc["lohFiles"].append(folder)
+                facets = "{wd}/{folder}_FACETS/facets_comp_cncf.tsv".format(wd = workindir, folder = analysisdir)
+                if os.path.isfile(facets) :
+                    auxDc["lohFiles"].append(facets)
+                ascatngs = lib.findAscatName("{wd}/{folder}_ASCAT/".format(wd = workindir, folder = analysisdir))
+                if ascatngs != "Not found" :
+                    auxDc["lohFiles"].append(ascatngs)
+                sequenza = "{wd}/{folder}_Sequenza/{case}_segments.txt".format(folder = analysisdir, case = c[0], wd = workindir)
+                if os.path.isfile(sequenza) :
+                    auxDc["lohFiles"].append(sequenza)
+                if len(auxDc["vcfFiles"]) > len(submitter["vcfFiles"]) and len(auxDc["lohFiles"]) > len(submitter["vcfFiles"]) :
+                    submitter = auxDc.copy()
 
-def main() :
-    brca1 = ["17", 43044295, 43170245]
-    brca2 = ["13", 32315086, 32400266]
-    print("INFO: Checking BRCA1")
-    txt = doTest("BRCA1", brca1)
-    with open("strelka2_brca1_ascat_facets_ascatngs_sequenza.tsv", "w") as fi :
-        fi.write(txt)
-    print("INFO: Checking BRCA2")
-    txt = doTest("BRCA2", brca2)
-    with open("strelka2_brca2_ascat_facets_ascatngs_sequenza.tsv", "w") as fi :
-        fi.write(txt)
+        # Get the variants in BRCA1 and BRCA2 genes
+        if len(submitter["vcfFiles"]) == 2 :
+            gmmaf, gmnoMaf = getVariant(submitter["vcfFiles"][1], genename)
+            # IDEA: Podria fer multiprocessing en aquesta busqueda
+            #tmmaf, tmnoMaf = getVariant(submitter["vcfFiles"][0], "BRCA1")
+            # Classify the variants according to the pathogenicity
+            varClass = classifyVariants(gmmaf, gmnoMaf, maxMaf)
+            # Get LOH in the region
+            try :
+                prog1, loh1 = doLoh(submitter["lohFiles"][0], brcagene)
+                if varClass == "+" :
+                    dcPos[prog1][loh1] += 1
+                elif varClass == "-" :
+                    dcNeg[prog1][loh1] += 1
+                elif varClass == "?" :
+                    dcNeu[prog1][loh1] += 1
+            except IndexError :
+                prog1 = None
+                loh1 = None
+            try :
+                prog2, loh2 = doLoh(submitter["lohFiles"][1], brcagene)
+                if varClass == "+" :
+                    dcPos[prog2][loh2] += 1
+                elif varClass == "-" :
+                    dcNeg[prog2][loh2] += 1
+                elif varClass == "?" :
+                    dcNeu[prog2][loh2] += 1
+            except IndexError :
+                prog2 = None
+                loh2 = None
+            try :
+                prog3, loh3 = doLoh(submitter["lohFiles"][2], brcagene)
+                if varClass == "+" :
+                    dcPos[prog3][loh3] += 1
+                elif varClass == "-" :
+                    dcNeg[prog3][loh3] += 1
+                elif varClass == "?" :
+                    dcNeu[prog3][loh3] += 1
+            except IndexError :
+                prog3 = None
+                loh3 = None
+            try :
+                prog4, loh4 = doLoh(submitter["lohFiles"][3], brcagene)
+                if varClass == "+" :
+                    dcPos[prog4][loh4] += 1
+                elif varClass == "-" :
+                    dcNeg[prog4][loh4] += 1
+                elif varClass == "?" :
+                    dcNeu[prog4][loh4] += 1
+            except IndexError :
+                prog4 = None
+                loh4 = None
+            # Count the number of each
+            if varClass == "+" :
+                totalPos += 1
+            elif varClass == "-" :
+                totalNeg += 1
+            elif varClass == "?" :
+                totalNeu += 1
+            # # # IDEA: Podria fer multiprocessing en la busqueda de cada arxiu LOH
+            # print("{} -> {} - {}".format(submitter["lohFiles"][0], prog1, loh1))
+            # print("{} -> {} - {}".format(submitter["lohFiles"][1], prog2, loh2))
+            # print("{} -> {} - {}".format(submitter["lohFiles"][2], prog3, loh3))
+            # print(dcPos)
 
+    print("\nINFO: Final results. {} were able to analyse".format(totalNeg + totalNeu + totalPos))
+    if vcPath.find("platypusGerm") > 0 :
+        print("INFO: Using Platypus as variant caller")
+    elif vcPath.find("strelkaGerm") > 0 :
+        print("INFO: Using Strelka2 as variant caller")
+    print("{} cases considered positive in {}".format(totalPos, genename))
+    for k in dcPos.keys() :
+        print("\t{} ({} analyses)".format(k, sum(dcPos[k].values())))
+        for key, value in dcPos[k].items() :
+            print("\t\t{} -> {} found".format(key, value))
+        print("\t\t\t{:.2f}% LOH".format(100 * (dcPos[k]["D"] + dcPos[k]["L"])/totalPos))
 
-if __name__ == "__main__" :
-    main()
+    print("\n{} cases considered negative in {}".format(totalNeg, genename))
+    for k in dcNeg.keys() :
+        print("\t{} ({} analyses)".format(k, sum(dcNeg[k].values())))
+        for key, value in dcNeg[k].items() :
+            print("\t\t{} -> {} found".format(key, value))
+        print("\t\t\t{:.2f}% LOH".format(100 * (dcNeg[k]["D"] + dcNeg[k]["L"])/totalNeg))
+
+    print("\n{} cases considered unknown in {}".format(totalNeu, genename))
+    for k in dcNeu.keys() :
+        print("\t{} ({} analyses)".format(k, sum(dcNeu[k].values())))
+        for key, value in dcNeu[k].items() :
+            print("\t\t{} -> {} found".format(key, value))
+        print("\t\t\t{:.2f}% LOH".format(100 * (dcNeu[k]["D"] + dcNeu[k]["L"])/totalNeu))
+
+brca1 = ["17", 43044295, 43170245]
+brca2 = ["13", 32315086, 32400266]
+maxMaf = 0.05
+variantCallingFile = "{}/platypusGerm/platypus.hg38_multianno.txt"
+#variantCallingFile = "{}/strelkaGerm/results/variants/strelka.hg38_multianno.txt"
+main(brca1, "BRCA1", variantCallingFile, maxMaf)
