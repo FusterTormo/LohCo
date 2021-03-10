11a2a5a56b27de18c610a9f3a57f9e6ad549c05f
fustertormo <emailserios@gmail.com>
2021-03-10 16:40:01 +0100

    Added statistics

--- main9.py
+++ main9.py
@@ -12,11 +12,11 @@ cFolder = "fsupek_cancer2"
 cancer = "OV" # Cancer repository to search
 dbcon = sqlite3.connect("/g/strcombio/fsupek_cancer2/TCGA_bam/info/info.db")
 
-wd = "/g/strcombio/cancer_path/TCGA_bam/{c}".format(c = cancer, cancer_path = cFolder)
+wd = "/g/strcombio/{cancer_path}/TCGA_bam/{c}".format(c = cancer, cancer_path = cFolder)
 
 # Stats
-cont = 0
-
+pairs = 0
+done = 0
 
 
 # Get the submitter IDs from the cancer repository
@@ -37,17 +37,32 @@ for c in cases :
 
     for tm in tumors :
         for cn in controls :
-            cont += 1
+            pairs += 1
+            loh = [] # Store the LOH reported by the tools
+            prefix = "{}_VS_{}".format(tm[0].split("-")[0], cn[0].split("-")[0])
+            # Get FACETS output file
+            folder = "{wd}/{sub}/{pre}_FACETS".format(wd = wd, sub = c[0], pre = prefix)
+            file = "{fld}/facets_comp_cncf.tsv".format(fld = folderº)
+            if os.path.isfile(folder) :
+                # Check LOH in the gene, add the output to a list
+                loh.append("L")
+            # Get Sequenza output file
+            folder = "{wd}/{sub}/{pre}_Sequenza".format(wd = wd, sub = c[0], pre = prefix)
+            file = "{fld}/{case}_segments.txt".format(fld = folder, case = c[0])
+            if os.path.isfile(file) :
+                # Check LOH in the gene, add th output to a list
+                loh.append("L")
+            # Get ASCAT2 output file
+            folder = "{wd}/{sub}/ASCAT2".format(wd = wd, sub = c[0])
+            if os.path.isfolder(folder) :
+                # Check LOH in the gene, add the output to a list
+                loh.append("L")
+            if len(loh) == 3 :
+                done += 1
 
-        # Get FACETS output file
 
-        # Check LOH in the gene, add the output to a list
-
-        # Get Sequenza output file
-        # Check LOH in the gene, add th output to a list
-        # Get ASCAT2 output file
-        # Check LOH in the gene, add the output to a list
         # Count the number of LOH found in the patient
         # https://appdividend.com/2021/02/08/python-list-count-how-to-count-elements-in-python-list/
 # Check if FACETS/Sequenza/ASCAT2 have reported LOH
-print("INFO: Total comparison: {}".format(cont))
+print("INFO: Pairs tumor-control: {}".format(pairs))
+print("INFO: Analysis done in {} pairs".format(done))
