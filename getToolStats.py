d152d40ae373ce96c1193b34af572d47738faba5
fustertormo <emailserios@gmail.com>
2021-04-27 15:58:20 +0200

    Created a script that counts the number of analyses done successfully using the tools pàssed as parameter, in a cancer passed as parameter

---
+++ getToolStats.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+MAIN: Get, from a cancer passed as parameter:
+    * number of pairs tumor-control available
+    * number of analyses succeed
+    * number of analyses failed
+"""
+
+cancers = {"OV" : "/g/strcombio/fsupek_cancer2/TCGA_bam", "BRCA" : "/g/strcombio/fsupek_cancer3/TCGA_bam"}
+tools = {
+    "FACETS" : {"file" : "facets_comp_cncf.tsv", "suffix" : "_FACETS"},
+    "ascatNGS" : {"file" : "copynumber.caveman.csv", "suffix" : "_ASCAT"},
+    "Sequenza" : {"file" : "_segments.txt", "suffix" : "_Sequenza"},
+    "PURPLE" : {"file" : "TUMOR.purple.cnv.gene.tsv", "suffix" : "_PURPLE"}
+}
+
+dbcon = sqlite3.connect("/g/strcombio/fsupek_cancer2/TCGA_bam/info/info.db")
+file = input("Which tool you want to check the analyses?\nOptions: ({}) ".format(" - ".join(tools.keys())))
+repo = input("Which cancer check the tool?\nOptions: ({}) ".format(" - ".join(cancers.keys())))1
+
+all = 0
+done = 0
+success = 0
+
+with dbcon :
+      cur = dbcon.cursor()
+      q = cur.execute("SELECT submitter FROM patient WHERE cancer='{cancer}'".format(cancer = cancer))
+      cases = q.fetchall()
+
+print("{} INFO: Total submitters in {} cancer: {}".format(getTime(), cancer, len(cases)))
+if file in tools.keys() and repo in cancers.keys() :
+    for c in cases :
+        with dbcon :
+            cur = dbcon.cursor()
+            q = cur.execute("SELECT uuid, bamName FROM sample WHERE submitter='{}' AND tumor LIKE '%Tumor%'".format(c[0]))
+            tumors = q.fetchall()
+            q = cur.execute("SELECT uuid, bamName FROM sample WHERE submitter='{}' AND tumor LIKE '%Normal%'".format(c[0]))
+            controls = q.fetchall()
+
+        for tm in tumors :
+            for cn in controls :
+                prefix = "{}_VS_{}".format(tm[0].split("-")[0], cn[0].split("-")[0])
+                wd = "{tmpath}/{cancer}/{prefix}{suffix}".format(tmpath = cancers[repo], cancer = repo, prefix = prefix, suffix = tool[file]["suffix"])
+                if os.path.isdir(wd) :
+                    done += 1
+                    if file == "ascatNGS" or file == "Sequenza" :
+                        aux = os.listdir(wd)
+                        for a in aux :
+                            if a.endswith(tools[file]["file"]) :
+                                print(a)
+                                success += 1
+                    elif file == "FACETS" or file == "PURPLE" :
+                        aux = "{}/{}".format(wd, tools[file]["file"])
+                        if os.path.isfile(aux) :
+                            success += 1
+else :
+    print("ERROR: Invalid option {} or {}".format(file, repo))
