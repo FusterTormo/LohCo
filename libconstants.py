ad6a4ab23bbd81f0f54ce669c8b890d4a95d93f7
FusterTormo <franfustor@gmail.com>
2021-06-06 22:12:23 +0200

    Converted the dict with chromosme length to a constant. Added it to libconstants

--- libconstants.py
+++ libconstants.py
@@ -9,3 +9,12 @@ aberrations = ["D", "L", "N", "A"]
 var_positive = ["nonframeshift deletion", "nonframeshift insertion", "frameshift substitution", "frameshift block substitution", "frameshift deletion", "frameshift insertion", "stopgain", "splicing"]
 var_neutral = ["nonsynonymous SNV", "nonframeshift substitution", "nonframeshift block substitution", "stoploss"]
 var_negative = ["NA", "synonymous SNV"]
+
+lenChromosomes = {"1" : 248956422, "2" : 242193529, "3" : 198295559,
+"4" : 190214555, "5" : 181538259, "6" : 170805979,
+"7" : 159345973, "8" : 145138636, "9" : 138394717,
+"10" : 133797422, "11" : 135086622, "12" : 133275309,
+"13" : 114364328, "14" : 107043718, "15" : 101991189,
+"16" : 90338345, "17" : 83257441, "18" : 80373285,
+"19" : 58617616, "20" : 64444167, "21" : 46709983,
+"22" : 50818468, "X" : 156040895, "Y" : 57227415}
