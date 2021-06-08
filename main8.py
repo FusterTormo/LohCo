043002f98c296d128e52e9c90800bfcfb6d70e33
fustertormo <francisco.fuster@irbbarcelona.org>
2021-06-08 16:34:29 +0200

    Fixed bug calling BRCA2 LOH analysis. Removed unnecessary verbosity

--- main8.py
+++ main8.py
@@ -449,7 +449,6 @@ def main(brcagene, genename, vcPath, maxMaf = 0.01) :
                 temp["p_{}".format(prog)] = purity
 
             data.append(temp)
-            print(temp)
 
 
     print("\nINFO: Final results. {} had a sample with both vcfs (tumor and control) done".format(cont))
@@ -510,5 +509,5 @@ if __name__ == "__main__" :
     variantCallingFile = "{}/platypusGerm/platypus.hg38_multianno.txt"
     #variantCallingFile = "{}/strelkaGerm/results/variants/strelka.hg38_multianno.txt"
     main(brca1, "BRCA1", variantCallingFile, maxMaf)
-    main(brca2, "BRCA2", variantCallingFile, maxMAF)
+    main(brca2, "BRCA2", variantCallingFile, maxMaf)
     print("INFO: Run main8.R with the output files *LOH.tsv to get the final results and to create the plots")
