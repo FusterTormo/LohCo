36e8f4ff71d68c77e78cc0c2e3ba0bcae7401a65
FusterTormo <franfustor@gmail.com>
2021-06-16 19:31:00 +0200

    Added instructions to do the same plots, but using DNAcopy as truthset

--- heatmapInput.py
+++ heatmapInput.py
@@ -31,7 +31,8 @@ def getJCC(sub, fil) :
 """Main program"""
 
 dbcon = sqlite3.connect("/g/strcombio/fsupek_cancer2/TCGA_bam/info/info.db")
-line = "submitter\tascat2\tdnacopy\tfacets\tascatngs\tsequenza\tpurple\n"
+ascatline = "submitter\tascat2\tdnacopy\tfacets\tascatngs\tsequenza\tpurple\n"
+arrayline = "submitter\tascat2\tdnacopy\tfacets\tascatngs\tsequenza\tpurple\n"
 # Get submitters list
 with dbcon :
       cur = dbcon.cursor()
@@ -40,15 +41,29 @@ with dbcon :
 
 for c in cases :
     # For each submitter, extract the best JCC
+    ascat = getJCC(c[0], "main6/arrayVSascat2.tsv")
+    dnacopy = getJCC(c[0], "main6/arrayVSarray.tsv")
+    facets = getJCC(c[0], "main6/arrayVSfacets.tsv")
+    ascatngs = getJCC(c[0], "main6/arrayVSascatNGS.tsv")
+    sequenza = getJCC(c[0], "main6/arrayVSsequenza.tsv")
+    purple = getJCC(c[0], "main6/arrayVSpurple.tsv")
+    arrayline += "{sub}\t{a}\t{d}\t{f}\t{n}\t{s}\t{p}\n".format(sub = c[0], a = ascat, d = dnacopy, f = facets, n = ascatngs, s = sequenza, p = purple)
+
     ascat = getJCC(c[0], "main6/ascat2VSascat2.tsv")
     dnacopy = getJCC(c[0], "main6/ascat2VSarray.tsv")
     facets = getJCC(c[0], "main6/ascat2VSfacets.tsv")
     ascatngs = getJCC(c[0], "main6/ascat2VSascatNGS.tsv")
     sequenza = getJCC(c[0], "main6/ascat2VSsequenza.tsv")
     purple = getJCC(c[0], "main6/ascat2VSpurple.tsv")
-    line += "{sub}\t{a}\t{d}\t{f}\t{n}\t{s}\t{p}\n".format(sub = c[0], a = ascat, d = dnacopy, f = facets, n = ascatngs, s = sequenza, p = purple)
+    ascatline += "{sub}\t{a}\t{d}\t{f}\t{n}\t{s}\t{p}\n".format(sub = c[0], a = ascat, d = dnacopy, f = facets, n = ascatngs, s = sequenza, p = purple)
+
+with open("main6/dnacopy_heatmap.tsv", "w") as fi :
+    fi.write(arrayline)
+
+print("INFO: Data written as main6/dnacopy_heatmap.tsv")
 
 with open("main6/ascat2_heatmap.tsv", "w") as fi :
-    fi.write(line)
+    fi.write(ascatline)
 
 print("INFO: Data written as main6/ascat2_heatmap.tsv")
+print("INFO: Run pcaMain6.R to get PCA and heatmap from this data")
--- pcaMain6.R
+++ pcaMain6.R
@@ -1,5 +1,6 @@
 library(ggbiplot)
 
+# Do PCA regarding JCC obtained using ASCAT2 as truth set
 data <- read.table("ascat2_heatmap.tsv", sep = "\t", header = TRUE)
 data.clean <- data[!is.na(data$ascat2) & !is.na(data$dnacopy) & !is.na(data$facets) & !is.na(data$ascatngs) & !is.na(data$sequenza) & !is.na(data$purple),]
 mt <- matrix(c(data.clean$ascat2, data.clean$dnacopy, data.clean$facets, data.clean$ascatngs, data.clean$sequenza, data.clean$purple), ncol = 6)
@@ -12,7 +13,24 @@ heatmap(mt)
 mt <- matrix(c(data.clean$dnacopy, data.clean$facets, data.clean$ascatngs, data.clean$sequenza, data.clean$purple), ncol = 5)
 rownames(mt) <- data.clean$submitter
 colnames(mt) <- c("DNAcopy", "FACETS", "ascatNGS", "Sequenza", "PURPLE")
+heatmap(mt)
 mt.pca <- prcomp(mt, center = TRUE, scale. = TRUE)
 ggbiplot(mt.pca, obs.scale = TRUE) + theme_minimal() + ggtitle("Jaccard Index compared with ASCAT2")
-ggbiplot(mt.pca, obs.scale = T, labels = rownames(mt))
+#ggbiplot(mt.pca, obs.scale = T, labels = rownames(mt))
 
+# Do PCA regarding JCC obtained using ASCAT2 as truth set
+data <- read.table("array_heatmap.tsv", sep = "\t", header = TRUE)
+data.clean <- data[!is.na(data$ascat2) & !is.na(data$dnacopy) & !is.na(data$facets) & !is.na(data$ascatngs) & !is.na(data$sequenza) & !is.na(data$purple),]
+mt <- matrix(c(data.clean$ascat2, data.clean$dnacopy, data.clean$facets, data.clean$ascatngs, data.clean$sequenza, data.clean$purple), ncol = 6)
+rownames(mt) <- data.clean$submitter
+colnames(mt) <- c("ASCAT2", "DNAcopy", "FACETS", "ascatNGS", "Sequenza", "PURPLE")
+# Plot heatmap
+heatmap(mt)
+# Do tool PCA
+# First remove DNAcopy column as it has no variance and raises problems when standardizing the data
+mt <- matrix(c(data.clean$ascat2, data.clean$facets, data.clean$ascatngs, data.clean$sequenza, data.clean$purple), ncol = 5)
+rownames(mt) <- data.clean$submitter
+colnames(mt) <- c("ASCAT2", "FACETS", "ascatNGS", "Sequenza", "PURPLE")
+heatmap(mt)
+mt.pca <- prcomp(mt, center = TRUE, scale. = TRUE)
+ggbiplot(mt.pca, obs.scale = TRUE) + theme_minimal() + ggtitle("Jaccard Index compared with DNAcopy")
