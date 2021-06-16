1c99c1ca478e69e1730df8561d059334492c74ef
FusterTormo <franfustor@gmail.com>
2021-06-16 11:10:57 +0200

    Created a script that extracts the information needed to do a heatmap with main6 data

---
+++ heatmapInput.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Extracts the best Jaccard Index for each submitter"""
+
+import subprocess
+
+def JCC(sub, fil) :
+    cmd = "grep {} {}".format(sub, fil)
+    pr = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    std, err = pr.communicate()
+    out = std.decode().strip().split("\n")
+    header = True
+    col = -1
+    dat = -1
+    for l in out :
+        aux = l.split("\t")
+        if header :
+            col = aux.index("JCC")
+            print("Index found: {}".format(col))
+            header = False
+        else :
+            try :
+                dat = float(aux[col])
+            except ValueError :
+                pass
+
+    if dat == -1 :
+        dat = "NA"
+    print("Returned: {} in {}".format(dat, fil))
+
+    return dat
+
+# Get submitters list
+with dbcon :
+      cur = dbcon.cursor()
+      q = cur.execute("SELECT submitter FROM patient WHERE cancer='OV'")
+      cases = q.fetchall()
+
+for c in cases :
+    # For each submitter, extract the best JCC
+    ascat = getJCC(c[0], "main6/ascat2VSascat2.tsv")
+    dnacopy = getJCC(c[0], "main6/ascat2VSarray.tsv")
+    facets = getJCC(c[0], "main6/ascat2VSfacets.tsv")
+    ascatngs = getJCC(c[0], "main6/ascat2VSascatNGS.tsv")
+    sequenza = getjcc(c[0], "main6/ascat2VSsequenza.tsv")
+    purple = getJcc(c[0], "main6/ascat2VSpurple.tsv")
+    break
