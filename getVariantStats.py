242b33a8c44a5cab741f2f1ae77f3ed31f474f85
fustertormo <emailserios@gmail.com>
2021-07-06 16:18:28 +0200

    Added flexibility for the gene to select

--- getVariantStats.py
+++ getVariantStats.py
@@ -12,17 +12,25 @@ import os
 
 cancers = {"OV" : "/g/strcombio/fsupek_cancer2/TCGA_bam", "BRCA" : "/g/strcombio/fsupek_cancer3/TCGA_bam"}
 
-dbcon = sqlite3.connect("/g/strcombio/fsupek_cancer2/TCGA_bam/info/info.db")
-repo = input("Which cancer check the tool?\nOptions: ({}) ".format(" - ".join(cancers.keys())))
+def main() :
+    dbcon = sqlite3.connect("/g/strcombio/fsupek_cancer2/TCGA_bam/info/info.db")
+    repo = input("Which cancer check the tool?\nOptions: ({}) ".format(" - ".join(cancers.keys())))
+    gene = input("Get the variants from which gene? ")
 
-# Get the submitter and uuid from the cancer asked
-with dbcon :
-    cur = dbcon.cursor()
-    q = cur.execute("SELECT s.submitter, uuid FROM patient p join sample s on s.submitter=p.submitter WHERE cancer='{cancer}'".format(cancer = repo))
-    cases = q.fetchall()
+    # Get the submitter and uuid from the cancer asked
+    with dbcon :
+        cur = dbcon.cursor()
+        q = cur.execute("SELECT s.submitter, uuid FROM patient p join sample s on s.submitter=p.submitter WHERE cancer='{cancer}'".format(cancer = repo))
+        cases = q.fetchall()
 
-for c in cases :
-    dirP = "{wd}/{cancer}/{sub}/{uuid}/platypusGerm".format(wd = cancers[repo], cancer = repo, sub = c[0], uuid = c[1])
-    dirS = "{wd}/{cancer}/{sub}/{uuid}/strelkaGerm".format(wd = cancers[repo], cancer = repo, sub = c[0], uuid = c[1])
-    if os.path.isdir(dirP) :
-        print("WARNING: {} not found".format(dirP))
+    for c in cases :
+        ficP = "{wd}/{cancer}/{sub}/{uuid}/platypusGerm/platypus.hg38_multianno.txt".format(wd = cancers[repo], cancer = repo, sub = c[0], uuid = c[1])
+        ficS = "{wd}/{cancer}/{sub}/{uuid}/strelkaGerm/results/variants/strelka.hg38_multianno.txt".format(wd = cancers[repo], cancer = repo, sub = c[0], uuid = c[1])
+        if os.path.isfile(ficP) : # Check if folder exists
+            cmd = "grep {gene} {file}".format(gene = gene, file = ficP)
+            print(cmd)
+        else :
+            print("WARNING: {} not found".format(dirP))
+
+if __name__ == "__main__" :
+    main()
