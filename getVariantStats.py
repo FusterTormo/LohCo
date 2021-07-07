9f8249408fe67e5eecbdb21c30277ca2f6816289
fustertormo <emailserios@gmail.com>
2021-07-07 09:43:05 +0200

    Added Strelka2 variant check

--- getVariantStats.py
+++ getVariantStats.py
@@ -50,9 +50,31 @@ def main() :
                             ppos += 1
                         elif aux[8] in cte.var_negative :
                             pneg += 1
-
         else :
             print("WARNING: {} not found".format(ficP))
+        if os.path.isfile(ficS) :
+            cmd = "grep {gene} {file}".format(gene = gene, file = ficS)
+            pr = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            std, err = pr.communicate()
+            out = std.decode().strip().split("\n")
+            for o in out :
+                sall += 1
+                aux = o.split("\t")
+                if len(aux) > 1 :
+                    if aux[5].find("splicing") > 0 :
+                        spos += 1
+                    else :
+                        if aux[8] in cte.var_positive :
+                            spos += 1
+                        elif aux[8] in cte.var_negative :
+                            sneg += 1
+        else :
+            print("WARNING: {} not found".format(ficS))
+        print(cmd)
+        print("{} variants. {} positive, {} negative".format(pall, ppos, pneg))
+        print(cmd)
+        print("{} variants. {} positive, {} negative".format(sall, spos, sneg))
+        break
 
 if __name__ == "__main__" :
     main()
