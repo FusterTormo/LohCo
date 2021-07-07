89bb2d08ab3bfa02df3ea4ebe9f43e1893c7817d
fustertormo <emailserios@gmail.com>
2021-07-07 16:35:57 +0200

    Variants divided in tumors and controls

--- getVariantStats.py
+++ getVariantStats.py
@@ -19,24 +19,34 @@ def main() :
     dbcon = sqlite3.connect("/g/strcombio/fsupek_cancer2/TCGA_bam/info/info.db")
     repo = input("Which cancer check the tool?\nOptions: ({}) ".format(" - ".join(cancers.keys())))
     gene = input("Get the variants from which gene? ")
-    pall = 0 # Number of variants reported by Platypus
-    ppos = 0
-    pneg = 0
-    sall = 0 # Number of variants reported by Strelka2
-    spos = 0
-    sneg = 0
+    platy = {"posT" : 0, "posC" : 0, "negT" : 0, "negC" : 0} # Variant classification in Platypus reported variants
+    elka = {"posT" : 0, "posC" : 0, "negT" : 0, "negC" : 0}
+    pall = 0 # Total variants detected by Platypus
+    sall = 0 # Total variants detected by Strelka2
+    tumors = 0 # Bam files corresponding to tumor samples
+    controls = 0 # Bam files corresponding to control samples
+    sample = ""
 
     # Get the submitter and uuid from the cancer asked
     with dbcon :
         cur = dbcon.cursor()
-        q = cur.execute("SELECT s.submitter, uuid FROM patient p join sample s on s.submitter=p.submitter WHERE cancer='{cancer}'".format(cancer = repo))
+        q = cur.execute("SELECT s.submitter, uuid, tumor FROM patient p join sample s on s.submitter=p.submitter WHERE cancer='{cancer}'".format(cancer = repo))
         cases = q.fetchall()
 
-    print("INFO: {} cases found".format(len(cases)))
+    print("INFO: {} bam files found".format(len(cases)))
     for c in cases :
+        if c[2].find("Tumor") > 0 :
+            tumors += 1
+            sample = "Tumor"
+        elif c[2].find("Normal") 0 :
+            controls += 1
+            sample = "Control"
+        else :
+            sample = "Undefined"
+
         if cases.index(c) % 100 == 0 :
-            print("{} INFO: {} cases executed".format(getTime(), cases.index(c)))
-            
+            print("INFO: {} cases executed: {} tumors, {} controls".format(cases.index(c), tumors, controls))
+
         ficP = "{wd}/{cancer}/{sub}/{uuid}/platypusGerm/platypus.hg38_multianno.txt".format(wd = cancers[repo], cancer = repo, sub = c[0], uuid = c[1])
         ficS = "{wd}/{cancer}/{sub}/{uuid}/strelkaGerm/results/variants/strelka.hg38_multianno.txt".format(wd = cancers[repo], cancer = repo, sub = c[0], uuid = c[1])
         if os.path.isfile(ficP) : # Check if platypus file exists
@@ -49,12 +59,21 @@ def main() :
                 aux = o.split("\t")
                 if len(aux) > 1 :
                     if aux[5].find("splicing") > 0 :
-                        ppos += 1
+                        if sample == "Tumor" :
+                            platy["posT"] += 1
+                        elif sample == "Control" :
+                            platy["posC"] += 1
                     else :
                         if aux[8] in cte.var_positive :
-                            ppos += 1
+                            if sample == "Tumor" :
+                                platy["posT"] += 1
+                            elif sample == "Control" :
+                                platy["posC"] += 1
                         elif aux[8] in cte.var_negative :
-                            pneg += 1
+                            if sample == "Tumor" :
+                                platy["negT"] += 1
+                            elif sample == "Control" :
+                                platy["negC"] += 1
         else :
             print("WARNING: {} not found".format(ficP))
 
@@ -68,16 +87,30 @@ def main() :
                 aux = o.split("\t")
                 if len(aux) > 1 :
                     if aux[5].find("splicing") > 0 :
-                        spos += 1
+                        if sample == "Tumor" :
+                            elka["posT"] += 1
+                        elif sample == "Control" :
+                            elka["posC"] += 1
                     else :
                         if aux[8] in cte.var_positive :
-                            spos += 1
+                            if sample == "Tumor" :
+                                elka["posT"] += 1
+                            elif sample == "Control" :
+                                elka["posC"] += 1
                         elif aux[8] in cte.var_negative :
-                            sneg += 1
+                            if sample == "Tumor" :
+                                elka["negT"] += 1
+                            elif sample == "Control" :
+                                elka["negC"] += 1
         else :
             print("WARNING: {} not found".format(ficS))
 
-    print("\t{} variants reported by Platypus:\n\t\t{} Positve\n\t\t{} Negative".format(pall, ppos, pneg))
-    print("\t{} variants reported by Strelka:\n\t\t{} Positve\n\t\t{} Negative".format(sall, spos, sneg))
+    print("SUMMARY\n-------")
+    print("Platypus reported {} variants in {} gene".format(pall, gene))
+    print("In TUMOR samples: {} Positive, {} Negative".format(platy["posT"], platy["negT"]))
+    print("In CONTROL samples: {} Positve, {} Negative\n".format(platy["posC"], platy["negC"]))
+    print("Strelka reported {} variants in {} gene".format(sall, gene))
+    print("In TUMOR samples: {} Positive, {} Negative".format(elka["posT"], elka["negT"]))
+    print("In CONTROL samples: {} Positve, {} Negative".format(elka["posC"], elka["negC"]))
 if __name__ == "__main__" :
     main()
